16cd3aa061c8fbcee474a1c669d8f5cc27e05fcd
Rafael García Gallego <rafael.garcia.gallego@gmail.com>
2012-02-07 18:40:09 +0100

    Basic follow controller, much work pending.

--- app/controllers/follow_controller.rb
+++ app/controllers/follow_controller.rb
@@ -1,7 +1,19 @@
 class FollowController < ApplicationController
   before_filter :authenticate_user!
+  before_filter :exclude_reflexive, :except => :index
+  load_and_authorize_resource :class => Relation::Follow
+
+  respond_to :html, :js
 
   def index
+    @followers = followers.all
+    @following = following.all
+
+    respond_to do |format|
+      format.html { @followers = @followers.page(params[:page]).per(10) }
+      format.js { @followers = @followers.page(params[:page]).per(10) }
+      format.json { render :text => to_json(@followers) }
+    end
   end
 
   def edit
@@ -11,6 +23,25 @@ class FollowController < ApplicationController
   end
 
   def destroy
+    
+  end
+
+  private
+
+  def exclude_reflexive
+    @contact = current_subject.sent_contacts.find params[:id]
+
+    if @contact.reflexive?
+      redirect_to home_path
+    end
+  end
+
+  def followers
+    current_subject.received_contacts
+  end
+
+  def following
+    current_subject.sent_contacts
   end
 
 end
