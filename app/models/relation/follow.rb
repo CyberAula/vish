4b1d0507c77e252d076be56d99d25b5d90f66bff
Antonio Tapiador del Dujo <atapiador@dit.upm.es>
2012-02-07 17:16:08 +0100

    Create follow relation with the follow permission

--- app/models/relation/follow.rb
+++ app/models/relation/follow.rb
@@ -1,3 +1,9 @@
 class Relation::Follow < Relation::Single
+  class << self
+    def instance
+      first ||
+        create(:permissions => Array(Permission.find_or_create_by_action('follow')))
+    end
+  end
 end
 
