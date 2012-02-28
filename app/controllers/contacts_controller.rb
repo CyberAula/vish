2f63ed4fee3939ca7951284eb817923ea01179da
Rafael García Gallego <rafael.garcia.gallego@gmail.com>
2012-02-28 16:39:29 +0100

    Empty contacts controller. Now we depend on the follows controller to establish relations. The horror.

---
+++ app/controllers/contacts_controller.rb
@@ -0,0 +1,8 @@
+#
+# This controller exists in social_stream, but it's replaced by FollowsController in ViSH
+#
+# To avoid messing things up, and to have views which call this controller raise an exception,
+# we hereby declare this controller empty
+#
+class ContactsController < ApplicationController
+end
