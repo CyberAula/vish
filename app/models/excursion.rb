79d2a8dc03bc7fba591d6673a04138de7d2129a7
Rafael García Gallego <rafael.garcia.gallego@gmail.com>
2012-02-29 11:29:21 +0100

    Give excursions a title and description extracted from the json string

--- app/models/excursion.rb
+++ app/models/excursion.rb
@@ -1,7 +1,20 @@
 class Excursion < ActiveRecord::Base
   include SocialStream::Models::Object
 
+  validates_presence_of :json
+  before_save :parse_for_meta
+
   def to_json
     json
   end
+
+  private
+
+  def parse_for_meta
+    parsed_json = ActiveSupport::JSON.decode(json)
+    activity_object.title = parsed_json["name"]
+    activity_object.description = parsed_json["description"]
+    activity_object.save!
+  end
+
 end
