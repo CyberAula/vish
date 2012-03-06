da1613bf194d54769d34ff174cbfd83e575044dc
Rafael García Gallego <rafael.garcia.gallego@gmail.com>
2012-03-06 18:25:51 +0100

    Dumb home helper to help design the home screen.

---
+++ app/helpers/home_helper.rb
@@ -0,0 +1,10 @@
+module HomeHelper
+  def current_actor_excursions
+    # TODO: really take the current user excursions
+    Excursion.all[0..3]
+  end
+  def current_actor_documents
+    # TODO: really take the current user excursions
+    Document.all[0..3]
+  end
+end
