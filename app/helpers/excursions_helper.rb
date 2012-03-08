6b7f596113c511f48a7a70402c0b2dd3893d0c32
Rafael García Gallego <rafael.garcia.gallego@gmail.com>
2012-03-08 14:20:07 +0100

    Add num_slides helper

--- app/helpers/excursions_helper.rb
+++ app/helpers/excursions_helper.rb
@@ -2,4 +2,9 @@ module ExcursionsHelper
   def thumb_for(excursion, size)
     image_tag excursion.thumb(size, self)
   end
+
+  def num_slides(excursion)
+    return 0 if excursion.nil?
+    excursion.slide_count.to_s
+  end
 end
