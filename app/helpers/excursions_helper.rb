f9ead14e04254fea8880fdfe53332b4d8045a5f6
Diego Carrera <diegocarrera2000@gmail.com>
2012-03-15 19:13:28 +0100

    home excursion & css

--- app/helpers/excursions_helper.rb
+++ app/helpers/excursions_helper.rb
@@ -6,4 +6,26 @@ module ExcursionsHelper
   def num_slides(excursion)
     excursion.slide_count.to_s
   end
+
+  def num_followers
+    # TODO: really take the top 10 excursions
+    rand(500)
+  end
+
+  def type_excursion
+    # TODO: really take the top 10 excursions
+    value=0
+    value=rand(2)
+    if (value==1)
+      "virtual meeting"
+    else
+      "presentation"
+    end
+  end
+
+  def starts
+    # TODO: really take the top 10 excursions
+    value=1 + (10)
+  end
+
 end
