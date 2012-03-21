28145207f5a5d07f742d2f59688ee01d7dc3c18d
Diego Carrera <diegocarrera2000@gmail.com>
2012-03-21 19:57:18 +0100

    add repository icons & helper

--- app/helpers/repository_helper.rb
+++ app/helpers/repository_helper.rb
@@ -1,9 +1,9 @@
 module RepositoryHelper
 
   def icon_class_for(document)
-    return 'icon-unknown' unless document.respond_to? :type
-    return 'icon-document' if document.type.nil?
-    "icon-#{document.type.downcase}"
+    return 'iconx-default' unless document.respond_to? :type
+    return 'iconx-default' if document.type.nil?
+    "iconx-#{document.type.downcase}"
   end
 
 end
