542c6f1558e073bbe8139a0a1113aa410099d648
Antonio Tapiador del Dujo <atapiador@dit.upm.es>
2012-04-10 16:58:47 +0200

    Home excursion queries
    fixes #17

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,12 +1,39 @@
 module HomeHelper
-  def current_actor_excursions(limit=4)
-    Excursion.authored_by(current_subject).order('updated_at DESC').first(limit)
-  end
-  def current_actor_documents(limit=4)
-    actor_documents
+  def current_subject_excursions(options = {})
+    subject_excursions current_subject, options
   end
 
-  def actor_documents(actor = current_subject, limit = 4)
-    Document.authored_by(actor).order('updated_at DESC').first(limit)
+  # Excursions can be search in several scopes:
+  # * :net subject and followings
+  # * :more everybody except subject and followings
+  def subject_excursions(subject, options = {})
+    subject_content subject, Excursion, options
+  end
+
+  def current_subject_documents(options = {})
+    subject_documents current_subject, options
+  end
+
+  def subject_documents(subject, options = {})
+    subject_content subject, Document, options
+  end
+
+  def subject_content(subject, klass, options = {})
+    options[:limit] ||= 4
+    options[:scope] ||= :net
+
+    following_ids = subject.following_actor_ids
+    following_ids |= [ subject.actor_id ]
+
+    query = klass
+
+    case options[:scope]
+    when :net
+      query = query.authored_by(following_ids)
+    when :more
+      query = query.not_authored_by(following_ids)
+    end
+
+    query.order('updated_at DESC').first(options[:limit])
   end
 end
