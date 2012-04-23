a755600e0446c47c9a10d15a60ffc67147e53f06
Antonio Tapiador del Dujo <atapiador@dit.upm.es>
2012-04-23 16:55:40 +0200

    Finish AJAX support

--- app/assets/javascripts/vish.search.js
+++ app/assets/javascripts/vish.search.js
@@ -1,10 +1,42 @@
 //= require vish
 
 Vish.Search = function(V, $, undefined){
-	var init = function(){
-		$('#tab').children('li')
+	var index = function(options){
+		$('#tab').children('li').each(function(){
+			if ($(this).hasClass('disabled')) {
+				return;
+			}
+
+			var anchor = $(this).find('a').attr('href').replace('#', '');
+			if (anchor == 'all') {
+				return;
+			}
+
+			$(this).click(function(){
+				var indexPath = options['indexPath'];
+				var typeParam = 'type=' + anchor;
+				var query;
+
+				if (indexPath.match(/type=/)) {
+					query = indexPath.replace(/type=\w*/, typeParam);
+				} else {
+					if (indexPath.match(/\?/)) {
+						query = indexPath + '&' + typeParam;
+					} else {
+						query = indexPath + '?' + typeParam;
+					}
+				}
+
+				$.ajax({
+					url: query,
+					dataType: 'script'
+				});
+			});
+
+		});
         }
 
 	return {
+		index: index
 	}
 }(Vish, jQuery);
