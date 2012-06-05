0202b708966fa2c097f1dc416b6bc0eabd601bef
Raquel García <raquel.garcia.bautista@gmail.com>
2012-06-05 17:29:21 +0200

    Fix notifications email

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -1,3 +1,3 @@
 # Turn off auto TLS for e-mail
-ActionMailer::Base.default_url_options[:host] = 'vishub.org'
+ActionMailer::Base.default_url_options[:host] = 'vishub.global.dit.upm.es'
 ActionMailer::Base.smtp_settings[:enable_starttls_auto] = false
