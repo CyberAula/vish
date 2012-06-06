3477dabe7da5b5b869780e46333ccb8dab022a25
Rafael García Gallego <rafael.garcia.gallego@gmail.com>
2012-06-06 15:44:05 +0200

    Repo helper kludge

--- app/helpers/repository_helper.rb
+++ app/helpers/repository_helper.rb
@@ -4,14 +4,24 @@ module RepositoryHelper
     return 'icon_x-link' if document.is_a? Link
     return 'iconx-default' unless document.respond_to? :type
     return 'iconx-default' if document.type.nil?
-    "iconx-#{document.type.downcase}"
+    case document.file.url.to_s
+    when /pdf$/
+      'iconx-pdf'
+    else
+      'iconx-default'
+    end
   end
 
   def icon75_class_for(document)
     return 'icon_75-link' if document.is_a? Link
     return 'icon75-default' unless document.respond_to? :type
     return 'icon75-default' if document.type.nil?
-    "icon75-#{document.type.downcase}"
+    case document.file.url.to_s
+    when /pdf$/
+      'icon75-pdf'
+    else
+      'icon75-default'
+    end
   end
 
 end
