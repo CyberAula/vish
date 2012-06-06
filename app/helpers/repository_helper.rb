0140be633166dff878624ae948336126e316c20d
Sandra Aguirre <saguiherrera@gmail.com>
2012-06-06 18:20:01 +0200

    icon type updated

--- app/helpers/repository_helper.rb
+++ app/helpers/repository_helper.rb
@@ -2,12 +2,52 @@ module RepositoryHelper
 
   def icon_class_for(document)
     return 'icon_x-link' if document.is_a? Link
-    return 'iconx-default' unless document.respond_to? :type
+    return 'icon_repository32-32_file' unless document.respond_to? :type
     case document.file.url.to_s.downcase[-3,3]
     when "pdf"
-      return 'iconx-pdf'
+      return 'icon_repository32-32_pdf'
+    when "mathml"  
+      return 'icon_repository32-32_mathml'
+    when "txt"  
+      return 'icon_repository32-32_txt'      
+    when "doc"  
+      return 'icon_repository32-32_word'
+    when "docx"  
+      return 'icon_repository32-32_word'      
+    when "ppt"  
+      return 'icon_repository32-32_ppt'      
+    when "xls"  
+      return 'icon_repository32-32_excel'      
+    when "audio"  
+      return 'icon_repository32-32_audio'      
+    when "mp3"  
+      return 'icon_repository32-32_mp3'      
+    when "wav"  
+      return 'icon_repository32-32_wav'      
+    when "picture"  
+      return 'icon_repository32-32_picture'      
+    when "tiff"  
+      return 'icon_repository32-32_tiff'      
+    when "jpg"  
+      return 'icon_repository32-32_jpg'      
+    when "gif"  
+      return 'icon_repository32-32_gif'      
+    when "svg"  
+      return 'icon_repository32-32_svg'      
+    when "zip"  
+      return 'icon_repository32-32_zip'      
+    when "rar"  
+      return 'icon_repository32-32_rar'      
+    when "avi"  
+      return 'icon_repository32-32_avi'      
+    when "mp4"  
+      return 'icon_repository32-32_mp4'      
+    when "mov"  
+      return 'icon_repository32-32_mov'      
+    when "swf"  
+      return 'icon_repository32-32_flash'      
     else
-      return 'iconx-default'
+      return 'icon_repository32-32_file'
     end
   end
 
@@ -16,9 +56,49 @@ module RepositoryHelper
     return 'icon75-default' unless document.respond_to? :type
     case document.file.url.to_s.downcase[-3,3]
     when "pdf"
-      return 'icon75-pdf'
+      return 'icon_repository75-75_pdf'
+    when "mathml"  
+      return 'icon_repository75-75_mathml'
+    when "txt"  
+      return 'icon_repository75-75_txt'      
+    when "doc"  
+      return 'icon_repository75-75_word'
+    when "docx"  
+      return 'icon_repository75-75_word'      
+    when "ppt"  
+      return 'icon_repository75-75_ppt'      
+    when "xls"  
+      return 'icon_repository75-75_excel'      
+    when "audio"  
+      return 'icon_repository75-75_audio'      
+    when "mp3"  
+      return 'icon_repository75-75_mp3'      
+    when "wav"  
+      return 'icon_repository75-75_wav'      
+    when "picture"  
+      return 'icon_repository75-75_picture'      
+    when "tiff"  
+      return 'icon_repository75-75_tiff'      
+    when "jpg"  
+      return 'icon_repository75-75_jpg'      
+    when "gif"  
+      return 'icon_repository75-75_gif'      
+    when "svg"  
+      return 'icon_repository75-75_svg'      
+    when "zip"  
+      return 'icon_repository75-75_zip'      
+    when "rar"  
+      return 'icon_repository75-75_rar'      
+    when "avi"  
+      return 'icon_repository75-75_avi'      
+    when "mp4"  
+      return 'icon_repository75-75_mp4'      
+    when "mov"  
+      return 'icon_repository75-75_mov'      
+    when "swf"  
+      return 'icon_repository75-75_flash'      
     else
-      return 'icon75-default'
+      return 'icon_repository75-75_file'
     end
   end
 
