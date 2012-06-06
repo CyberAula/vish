d9a9f7d50c4f1941813b11587d6c942aa3577bfb
Rafael García Gallego <rafael.garcia.gallego@gmail.com>
2012-06-06 16:23:02 +0200

    Litle fix

--- app/helpers/repository_helper.rb
+++ app/helpers/repository_helper.rb
@@ -3,24 +3,22 @@ module RepositoryHelper
   def icon_class_for(document)
     return 'icon_x-link' if document.is_a? Link
     return 'iconx-default' unless document.respond_to? :type
-    return 'iconx-default' if document.type.nil?
-    case document.file.url.to_s
-    when /pdf$/
-      'iconx-pdf'
+    case document.file.url.to_s.downcase[-3,3]
+    when "pdf"
+      return 'iconx-pdf'
     else
-      'iconx-default'
+      return 'iconx-default'
     end
   end
 
   def icon75_class_for(document)
     return 'icon_75-link' if document.is_a? Link
     return 'icon75-default' unless document.respond_to? :type
-    return 'icon75-default' if document.type.nil?
-    case document.file.url.to_s
-    when /pdf$/
-      'icon75-pdf'
+    case document.file.url.to_s.downcase[-3,3]
+    when "pdf"
+      return 'icon75-pdf'
     else
-      'icon75-default'
+      return 'icon75-default'
     end
   end
 
