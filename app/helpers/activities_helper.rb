aca95c9949c5db340e2b96c3ccb0a047e235f00f
Rafael García Gallego <rafael.garcia.gallego@gmail.com>
2012-06-21 11:28:31 +0200

    Big favorite stars

--- app/helpers/activities_helper.rb
+++ app/helpers/activities_helper.rb
@@ -15,7 +15,7 @@ module ActivitiesHelper
       params << if options[:size] == :small
                   image_tag("star-off10.png", :class => "menu_icon")
                 else
-                  image_tag("star-off.png", :class => "menu_icon")
+                  image_tag("star-off.png", :class => "menu_icon") + t('follow.add_favorite')
                 end
       params << new_user_session_path
       params << {:class => "verb_like like_size_" + options[:size].to_s,:id => "like_" + dom_id(object)}
@@ -24,18 +24,18 @@ module ActivitiesHelper
         params << if options[:size] == :small
                     image_tag("star-on10.png", :class => "menu_icon")
                   else
-                    image_tag("star-on.png", :class => "menu_icon")
+                    image_tag("star-on.png", :class => "menu_icon") + t('follow.is_favorite')
                   end
-        params << [object, :like]
-        params << {:class => "verb_like like_size_" + options[:size].to_s,:id => "like_" + dom_id(object),:method => :delete, :remote => true}
+        params << url_for(object) + '/like?size=' + options[:size].to_s
+        params << {:class => "verb_like",:id => "like_" + dom_id(object),:method => :delete, :remote => true}
       else
         params << if options[:size] == :small
                     image_tag("star-off10.png", :class => "menu_icon")
                   else
-                    image_tag("star-off.png", :class => "menu_icon")
+                    image_tag("star-off.png", :class => "menu_icon") + t('follow.add_favorite')
                   end
-        params << [object, :like]
-        params << {:class => "verb_like like_size_" + options[:size].to_s,:id => "like_" + dom_id(object),:method => :post, :remote => true}
+        params << url_for(object) + '/like?size=' + options[:size].to_s
+        params << {:class => "verb_like",:id => "like_" + dom_id(object),:method => :post, :remote => true}
       end
     end
   end
