b06649e558a009346a12b8a48909eef4dd194177
Rafael García Gallego <rafael.garcia.gallego@gmail.com>
2012-06-24 13:12:24 +0200

    Fix like issues

--- app/helpers/activities_helper.rb
+++ app/helpers/activities_helper.rb
@@ -18,7 +18,7 @@ module ActivitiesHelper
                   image_tag("star-off.png", :class => "menu_icon") + t('follow.add_favorite')
                 end
       params << new_user_session_path
-      params << {:class => "verb_like like_size_" + options[:size].to_s,:id => "like_" + dom_id(object)}
+      params << {:class => "verb_like like_size_" + options[:size].to_s + " like_" + dom_id(object)}
     else
       if (object.liked_by?(current_subject))
         params << if options[:size] == :small
@@ -26,16 +26,16 @@ module ActivitiesHelper
                   else
                     image_tag("star-on.png", :class => "menu_icon") + t('follow.is_favorite')
                   end
-        params << url_for(object) + '/like?size=' + options[:size].to_s
-        params << {:class => "verb_like",:id => "like_" + dom_id(object),:method => :delete, :remote => true}
+        params << [object, :like]
+        params << {:class => "verb_like like_size_" + options[:size].to_s + " like_" + dom_id(object),:method => :delete, :remote => true}
       else
         params << if options[:size] == :small
                     image_tag("star-off10.png", :class => "menu_icon")
                   else
                     image_tag("star-off.png", :class => "menu_icon") + t('follow.add_favorite')
                   end
-        params << url_for(object) + '/like?size=' + options[:size].to_s
-        params << {:class => "verb_like",:id => "like_" + dom_id(object),:method => :post, :remote => true}
+        params << [object, :like]
+        params << {:class => "verb_like like_size_" + options[:size].to_s + " like_" + dom_id(object),:method => :post, :remote => true}
       end
     end
   end
