b50ed15938d02a682fc445c2a5125810e19363ce
Rafael García Gallego <rafael.garcia.gallego@gmail.com>
2012-08-31 15:57:49 +0200

    Fix follow button duplicated in most cases.

--- app/assets/javascripts/social_stream.follow.js
+++ app/assets/javascripts/social_stream.follow.js
@@ -1,10 +1,12 @@
 SocialStream.Follow = (function(SS, $, undefined){
 	var initButtons = function(){
+		$(".following-button").off("mouseenter");
 		$(".following-button").mouseenter(function(){
 			$(this).hide();
 			$(this).siblings(".unfollow-button").show();
 		});
 
+		$(".unfollow-button").off("mouseenter");
 		$(".unfollow-button").mouseleave(function(){
 			$(this).hide();
 			$(this).siblings(".following-button").show();
