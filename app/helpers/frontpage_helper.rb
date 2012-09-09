9da88c370b1f1f7623db68f78b89139c4fbf65b4
Rafael García Gallego <rafael.garcia.gallego@gmail.com>
2012-09-09 16:08:55 +0200

    Better top-10 algo

--- app/helpers/frontpage_helper.rb
+++ app/helpers/frontpage_helper.rb
@@ -1,6 +1,6 @@
 module FrontpageHelper
   def current_top_actor_excursions
-    # We take visits for now...
-    Excursion.order{ e.visit_count }.first(10)
+    # We take visits and likes for now...
+    Excursion.order{ e.visit_count + (10 * e.like_count) }.first(10)
   end
 end
