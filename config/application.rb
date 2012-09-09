83d1036a3565954425d0b98be00defadddcb35a3
Rafael García Gallego <rafael.garcia.gallego@gmail.com>
2012-09-09 20:21:42 +0200

    First take at mobile search. For now, it uses the same formats as the timeline. Users and comments are still missing, posts can definitely be improved.

--- config/application.rb
+++ config/application.rb
@@ -63,7 +63,7 @@ module Vish
       LinksController.layout "3x9"
       ExcursionsController.layout "3x9"
       QuizSessionsController.layout "3x9"
-      SearchController.layout "3x9"
+      SearchController.layout "search"
       LegalNoticeController.layout "12"
       HelpController.layout "12"
 
