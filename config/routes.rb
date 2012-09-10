5b5b2e4fd13b373fbae924b714b6acb6046bfd05
Rafael García Gallego <rafael.garcia.gallego@gmail.com>
2012-09-10 11:02:24 +0200

    Temporarily remove redirections so forms work

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Vish::Application.routes.draw do
   # Blatant redirections
-  match '/links' => redirect('/documents'), :via => :get
-  match '/embeds' => redirect('/documents'), :via => :get
   match '/contacts' => redirect('/followings')
   match '/users/:id/links' => redirect('/users/%{id}/documents')
   match '/users/:id/embeds' => redirect('/users/%{id}/documents')
