9aa9e87a2567b0e6226d68d5ba8bd928ae673e72
Beatriz <beatriz@OptiPlex-745.(none)>
2012-10-19 12:43:13 +0200

    Big modals for documents credit to @AlvaroSB for half the work

--- app/assets/javascripts/vish.wall.js
+++ app/assets/javascripts/vish.wall.js
@@ -246,7 +246,7 @@ Vish.Wall = (function(V, $, undefined){
     if (modal.length) {
       return "";
     } else {
-      return $('<div class="modal hide" id="' + klass + '-modal-' + id + '"><div class="modal-header"><h3 class="text-center">' + title + '</h3></div><div id="'+klass+'-modal-body-'+id+'" class="modal-body text-center">'+ modalPayload(klass, id) +'</div><div class="modal-footer"><div class="pull-left">' + modalLikeBtn(signed_in, activity_id, is_fav) + '</div><div class="pull-right">' + modalNavBtns() + '<a href="#" class="btn btn-danger ' + klass + '-modal-close-' + id + '" data-dismiss="modal">'+ I18n.t('close') +'</a><a href="/' + klass + 's/' + id + '" class="btn btn-success">' + I18n.t('details.msg') + '</a></div></div></div>').appendTo($('body'));
+      return $('<div class="resize ajuste modal hide" id="' + klass + '-modal-' + id + '"><div class="modal-header"><h3 class="text-center">' + title + '</h3></div><div id="'+klass+'-modal-body-'+id+'" class="footar modal-body text-center">'+ modalPayload(klass, id) +'</div><div id="modyfooter" class="sticky modal-footer" ><div class="pull-left">' + modalLikeBtn(signed_in, activity_id, is_fav) + '</div><div class="pull-right">' + modalNavBtns() + '<a href="#" class="btn btn-danger ' + klass + '-modal-close-' + id + '" data-dismiss="modal">'+ I18n.t('close') +'</a><a href="/' + klass + 's/' + id + '" class="btn btn-success">' + I18n.t('details.msg') + '</a></div></div></div>').appendTo($('body'));
     }
   }
 
