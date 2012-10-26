96765af8a7f42ac8f44563729bf3e635a073e95b
Beatriz <beatriz@OptiPlex-745.(none)>
2012-10-26 17:36:47 +0200

    fixing modals for documents first step Half the work to @AlvaroSB

--- app/assets/javascripts/vish.officedoc.js
+++ app/assets/javascripts/vish.officedoc.js
@@ -1,56 +1,62 @@
 Vish.Officedoc = (function(V,undefined){
 
-var calculate = function(anch){
-	return -($(anch).width() / 2);
+  var calculate = function(anch){
+  	return -($(anch).width() / 2);
+  } 
 
-}
 
-var redim = function(anch){
+  var redim = function(anch){
+     console.log("he llamado a redim");
+    $('.resize').css({
+      width: 'auto',
+     'margin-left': calculate(anch)
+     });
+  }
 
-$('.resize').css({
-        width: 'auto',
-        'margin-left': calculate(anch)
-    });
-	
-}
 
-var googdoc = function(){
-  	var height = ($('.resize').height())*(8/10);
-  	setTimeout(function(){
-  	$('#gdoc').css("height", height);
-	},500);
-}
+  var googdoc = function(){
+    console.log("he entrado en googdoc");
+     
+    	var height = ($('.resize').height())*(8/10);
+    	setTimeout(function(){
+    	$('#gdoc').css("height", height);
+  	},500);
+  }
 
-var rwindow = function(){
-	var height = ($('.resize').height())*(9/10);
-  	$(window).resize(function () {
-  	var height = ($('.resize').height())*(8/10);
-  	$('#gdoc').css("height", height);
+  var rwindow = function(){
+    console.log("he entrado en rwindow");
+    
+  	var height = ($('.resize').height())*(9/10);
+    	$(window).resize(function () {
+    	var height = ($('.resize').height())*(8/10);
+    	$('#gdoc').css("height", height);
 
-});
+  });
 
-}
+  }
 
-var removeM = function(){
-      var divs_tmp2 = $("div[id^='picture-modal-']");  //resize
-      var divs_tmp1 = $("div[id^='picture-modal-body']");     //footar  
-      var divs_tmp3 = $("div[id^='modyfooter']");           //sticky
-      
-          console.log(divs_tmp3); //sticky
-          console.log(divs_tmp2); //resize
-          console.log(divs_tmp1); //footar  
+  var removeM = function(){
+    console.log("he entrado en removeM");
+        var divs_tmp2 = $("div[id^='picture-modal-']");  //resize
+        var divs_tmp1 = $("div[id^='picture-modal-body']");     //footar  
+        var divs_tmp3 = $("div[id^='modyfooter']");           //sticky
+        
+            console.log(divs_tmp3); //sticky
+            console.log(divs_tmp2); //resize
+            console.log(divs_tmp1); //footar  
 
-           divs_tmp1.removeClass('footar')
-           divs_tmp2.removeClass('resize')
-           divs_tmp3.removeClass('sticky')
+             divs_tmp1.removeClass('footar')
+             divs_tmp2.removeClass('resize')
+             divs_tmp3.removeClass('sticky')
 
-}
+  }
 
-return {
-  redim: redim,
-  rwindow : rwindow,
-  googdoc : googdoc,
-  removeM : removeM,
-}
+  return {
+    removeM : removeM,
+    redim : redim,
+    rwindow : rwindow,
+    googdoc : googdoc
+    
+  }
 
 }) (Vish);
