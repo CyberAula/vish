5db19829dbc3bbf256fee72754078913ed0b2d60
Antonio Tapiador del Dujo <atapiador@dit.upm.es>
2012-10-31 13:27:01 +0100

    Site config

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121030131504) do
+ActiveRecord::Schema.define(:version => 20121031120423) do
 
   create_table "activities", :force => true do |t|
     t.integer  "activity_verb_id"
@@ -402,6 +402,12 @@ ActiveRecord::Schema.define(:version => 20121030131504) do
   add_index "shortened_urls", ["owner_id", "owner_type"], :name => "index_shortened_urls_on_owner_id_and_owner_type"
   add_index "shortened_urls", ["unique_key"], :name => "index_shortened_urls_on_unique_key", :unique => true
   add_index "shortened_urls", ["url"], :name => "index_shortened_urls_on_url"
+
+  create_table "sites", :force => true do |t|
+    t.text     "config"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "slides", :force => true do |t|
     t.datetime "created_at",         :null => false
