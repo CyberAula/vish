9bac097d3e090210ed6bd108f620b11561c23072
Juanka <juanka0087@hotmail.com>
2012-11-13 18:46:23 +0100

    excursion

--- app/helpers/activities_helper.rb
+++ app/helpers/activities_helper.rb
@@ -37,26 +37,26 @@ module ActivitiesHelper
     params = Array.new
     if !user_signed_in?
       params << if options[:size] == :small
-                  image_tag("star-off10.png", :class => "menu_icon")
+                  image_tag("icons/star-off10.png", :class => "menu_icon")
                 else
-                  image_tag("star-off.png", :class => "menu_icon")
+                  image_tag("icons/star-off.png", :class => "menu_icon")
                 end
       params << new_user_session_path
       params << {:class => "verb_like like_size_" + options[:size].to_s + " like_" + dom_id(object)}
     else
       if (object.liked_by?(current_subject))
         params << if options[:size] == :small
-                    image_tag("star-on10.png", :class => "menu_icon")
+                    image_tag("icons/star-on10.png", :class => "menu_icon")
                   else
-                    image_tag("star-on.png", :class => "menu_icon")
+                    image_tag("icons/star-on.png", :class => "menu_icon")
                   end
         params << [object, :like]
         params << {:class => "verb_like like_size_" + options[:size].to_s + " like_" + dom_id(object),:method => :delete, :remote => true}
       else
         params << if options[:size] == :small
-                    image_tag("star-off10.png", :class => "menu_icon")
+                    image_tag("icons/star-off10.png", :class => "menu_icon")
                   else
-                    image_tag("star-off.png", :class => "menu_icon")
+                    image_tag("icons/star-off.png", :class => "menu_icon")
                   end
         params << [object, :like]
         params << {:class => "verb_like like_size_" + options[:size].to_s + " like_" + dom_id(object),:method => :post, :remote => true}
