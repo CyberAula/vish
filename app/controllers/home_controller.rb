28fdf40acc42c8423d4361643b6f4b1ead293698
Antonio Tapiador del Dujo <atapiador@dit.upm.es>
2012-11-15 15:19:05 +0100

    Home timewall

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,8 +2,9 @@ class HomeController < ApplicationController
   before_filter :authenticate_user!
 
   def index
-    @activities_net = current_subject.wall(:home).page(params[:page_net])
-    @activities_me = current_subject.wall(:profile, :for => current_subject).page(params[:page_me])
+# FIXME
+#    @activities_net = current_subject.wall(:home).page(params[:page_net])
+#    @activities_me = current_subject.wall(:profile, :for => current_subject).page(params[:page_me])
 
     respond_to :js, :html
   end
--- config/initializers/avatars_for_rails.rb
+++ config/initializers/avatars_for_rails.rb
@@ -2,9 +2,11 @@ AvatarsForRails.setup do |config|
   config.avatarable_model = :actor
   config.current_avatarable_object = :current_actor
   config.avatarable_filters = [:authenticate_user!]
-  config.avatarable_styles = { :representation => "16x16>",
-                                 :contact => "50x50>",
-                                 :actor => '25x25>',
-                                 :profile => '119x119'}
+  config.avatarable_styles = {
+    :'16' => "16x16>",
+    :'25' => '25x25>',
+    :'50' => "50x50>",
+    :'119' => '119x119'
+  }
 end
 
