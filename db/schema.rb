1d2e24cef1a1b1261dc7651f65d94bac37e6096f
Juanka <juanka0087@hotmail.com>
2012-11-19 13:17:06 +0100

    coments

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120920142058) do
+ActiveRecord::Schema.define(:version => 20121111211308) do
 
   create_table "activities", :force => true do |t|
     t.integer  "activity_verb_id"
@@ -226,6 +226,7 @@ ActiveRecord::Schema.define(:version => 20120920142058) do
     t.integer  "slide_count",        :default => 1
     t.string   "thumbnail_url",      :default => "/assets/logos/original/excursion-00.png"
     t.boolean  "draft",              :default => false
+    t.text     "offline_manifest"
   end
 
   create_table "groups", :force => true do |t|
@@ -248,6 +249,13 @@ ActiveRecord::Schema.define(:version => 20120920142058) do
   end
 
   add_index "links", ["activity_object_id"], :name => "index_links_on_activity_object_id"
+
+  create_table "live_sessions", :force => true do |t|
+    t.integer  "user_id"
+    t.integer  "excursion_id"
+    t.datetime "created_at",   :null => false
+    t.datetime "updated_at",   :null => false
+  end
 
   create_table "notifications", :force => true do |t|
     t.string   "type"
@@ -401,6 +409,12 @@ ActiveRecord::Schema.define(:version => 20120920142058) do
   add_index "shortened_urls", ["owner_id", "owner_type"], :name => "index_shortened_urls_on_owner_id_and_owner_type"
   add_index "shortened_urls", ["unique_key"], :name => "index_shortened_urls_on_unique_key", :unique => true
   add_index "shortened_urls", ["url"], :name => "index_shortened_urls_on_url"
+
+  create_table "sites", :force => true do |t|
+    t.text     "config"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "slides", :force => true do |t|
     t.datetime "created_at",         :null => false
