9777184aa265718bcb86cee8ab93acfd0ab0943a
Rafael García Gallego <rafael.garcia.gallego@gmail.com>
2012-12-08 18:05:40 +0100

    Fix excursion drafts in home

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -57,7 +57,8 @@ module HomeHelper
       query = query.not_authored_by(following_ids)
     end
 
-    query = query.where(:draft => false) if (klass == Excursion) and (options[:scope] == :net or options[:scope] == :more or (subject != current_subject and options[:scope] == :me))
+    # WARNING: if klass includes Excursion, well.... just don't do it :)
+    query = query.where(:draft => false) if (klass.include?(Excursion)) and (options[:scope] == :net or options[:scope] == :more or (subject != current_subject and options[:scope] == :me))
 
     query = query.order('activity_objects.updated_at DESC')
     query = query.limit(options[:limit]) if options[:limit] > 0
