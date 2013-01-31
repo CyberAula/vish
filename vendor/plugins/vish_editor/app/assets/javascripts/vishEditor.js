2c023447e7f5d6d5a18a793a9a0275a1b1df4c59
Aldo Gordillo <agordillos@gmail.com>
2013-01-31 18:19:51 +0100

    Integrating Vish Editor Version 0.03

--- vendor/plugins/vish_editor/app/assets/javascripts/vishEditor.js
+++ vendor/plugins/vish_editor/app/assets/javascripts/vishEditor.js
@@ -18092,7 +18092,7 @@ VISH.Editor.Quiz.Dummies = function(VISH, undefined) {
   var quizOptionsDummies = [];
   var init = function() {
     quizDummies = ["<div class='openQuizContainer'><textarea class='value_open_question_in_zone'><div><font size=" + 4 + ">Write question here</font></div></textarea></div>", "<div class='multipleChoiceQuizContainer'><div class='value_multiplechoice_question_in_zone'><div class='initTextDiv'><font size='4'>Write question here</font></div></div><ul class='ul_mch_options_in_zone'></ul><input type='hidden' name='quiz_id'/></div></div>", "<div class='trueFalseQuizContainer'><p> quiz dummy truefalse</p></div>"];
-    quizOptionDummies = ["", "<li class='li_mch_options_in_zone'><span class='quiz_option_index'></span><div class='multiplechoice_option_in_zone'><div class='initTextDiv'><font size='4'>Write options here</font></div></div><img src='" + VISH.ImagesPath + "icons/add.png' class='add_quiz_option_button'/><img src='" + VISH.ImagesPath + "icons/delete.png' class='delete_quiz_option_button'/></li>", ""]
+    quizOptionDummies = ["", "<li class='li_mch_options_in_zone'><span class='quiz_option_index'></span><div class='multiplechoice_option_in_zone'><div class='initTextDiv'><font size='4'>Write options here</font></div></div><img src='" + VISH.ImagesPath + "icons/add.png' class='add_quiz_option_button'/><img src='" + VISH.ImagesPath + "icons/ve_delete.png' class='delete_quiz_option_button'/></li>", ""]
   };
   var hashTypeQuiz = {"open":0, "multiplechoice":1, "truefalse":2};
   var getQuizDummy = function(type_quiz, position) {
