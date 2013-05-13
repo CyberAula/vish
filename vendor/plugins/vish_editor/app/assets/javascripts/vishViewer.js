06ad371be7d9e12bf180b8a960f3321a7e8dc9c1
Aldo Gordillo <agordillos@gmail.com>
2013-05-13 20:15:01 +0200

    Several bugs fixed

--- vendor/plugins/vish_editor/app/assets/javascripts/vishViewer.js
+++ vendor/plugins/vish_editor/app/assets/javascripts/vishViewer.js
@@ -10175,7 +10175,21 @@ VISH.Status = function(V, $, undefined) {
     return _isInIframe
   };
   var _checkDomain = function() {
-    _isAnotherDomain = _checkIframe() && typeof window.parent.location.href === "undefined";
+    _isAnotherDomain = false;
+    if(_checkIframe()) {
+      var parent = window.parent;
+      while(parent != window.top) {
+        if(typeof parent.location.href === "undefined") {
+          _isAnotherDomain = true;
+          break
+        }else {
+          parent = parent.parent
+        }
+      }
+      if(typeof window.top.location.href === "undefined") {
+        _isAnotherDomain = true
+      }
+    }
     return _isAnotherDomain
   };
   var _checkOnline = function() {
