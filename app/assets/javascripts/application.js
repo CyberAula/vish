afa4e66c38fd6af56c899d94bdffff77b32313c6
crispinh2o <crispinh2o@hotmail.com>
2013-06-26 03:46:59 +0200

    my room icon show

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,7 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
-//= require bootstrap.min
+//
 //= require jquery
 //= require jquery_ujs
 //= require social_stream-base
