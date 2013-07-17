7ccf325cae365858065f92eb4d0228363a6b7324
Enrique Barra <enrique.barra.arias@gmail.com>
2013-07-17 12:52:08 +0200

    new changes for excursion

--- app/helpers/excursions_helper.rb
+++ app/helpers/excursions_helper.rb
@@ -1,6 +1,9 @@
 module ExcursionsHelper
   def excursion_thumb_for(excursion, size)
-    logger.info("TITULO " + excursion.title)
+    excursion_thumb_for(excursion)
+  end
+
+  def excursion_thumb_for(excursion)
     return image_tag("icons/draft.png") if excursion.draft
     image_tag (excursion.thumbnail_url || "/assets/logos/original/excursion-00.png")
   end
