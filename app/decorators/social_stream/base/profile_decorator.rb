8d0e6bba63d1291efa293f59ce2c9b9486229cbb
holacarol <carolgarcia@dit.upm.es>
2013-08-01 17:24:02 +0200

    Add profile form

--- app/decorators/social_stream/base/profile_decorator.rb
+++ app/decorators/social_stream/base/profile_decorator.rb
@@ -1,12 +1,12 @@
 Profile.class_eval do
-  has_one 	:user, 
-  			:through => :actor,
-  			:autosave   => true
+  has_one   :user, 
+  :through => :actor,
+  :autosave   => true
 
   delegate :tag_list, :tag_list=,
-           to: :actor
+  to: :actor
 
   delegate :occupation_t, :occupation?,:occupation, :occupation=,
-           to: :user
+  to: :user
 
 end
