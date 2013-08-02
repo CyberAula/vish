0a5ecc69d96e854796f87faa6c1b3a92640d8512
juan carlos <juanka0087@hotmail.com>
2013-08-02 16:47:27 +0200

    info excursion and helper icons star

--- app/helpers/vish_activities_helper.rb
+++ app/helpers/vish_activities_helper.rb
@@ -46,26 +46,26 @@ module VishActivitiesHelper
     params = Array.new
     if !user_signed_in?
       params << if options[:size] == :small
-                  image_tag("icons/star-off10.png", :class => "menu_icon")
+                  link_to(raw("<i class='icon-star-empty '></i> "), :class => "")
                 else
-                  image_tag("icons/star-off.png", :class => "menu_icon")
+                  link_to(raw("<i class='icon-star-empty '></i> "), :class => "")
                 end
       params << new_user_session_path
       params << {:class => "verb_like like_size_" + options[:size].to_s + " like_" + dom_id(object)}
     else
       if (object.liked_by?(current_subject))
         params << if options[:size] == :small
-                    image_tag("icons/star-on10.png", :class => "menu_icon")
+                    link_to(raw("<i class='icon-star '></i> "), :class => "")
                   else
-                    image_tag("icons/star-on.png", :class => "menu_icon")
+                    link_to(raw("<i class='icon-star '></i> "), :class => "")
                   end
         params << [object, :like]
         params << {:class => "verb_like like_size_" + options[:size].to_s + " like_" + dom_id(object),:method => :delete, :remote => true}
       else
         params << if options[:size] == :small
-                    image_tag("icons/star-off10.png", :class => "menu_icon")
+                    link_to(raw("<i class='icon-star-empty '></i> "), :class => "")
                   else
-                    image_tag("icons/star-off.png", :class => "menu_icon")
+                    link_to(raw("<i class='icon-star-empty '></i> "), :class => "")
                   end
         params << [object, :like]
         params << {:class => "verb_like like_size_" + options[:size].to_s + " like_" + dom_id(object),:method => :post, :remote => true}
