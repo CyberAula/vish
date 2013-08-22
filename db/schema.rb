e87e3316af3b0c95ec1b904cb3b71bedb2ffbb46
Carolina Garcia <holacarol@gmail.com>
2013-08-22 21:30:02 +0200

    Create category object

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130801114520) do
+ActiveRecord::Schema.define(:version => 20130822182134) do
 
   create_table "activities", :force => true do |t|
     t.integer  "activity_verb_id"
@@ -138,6 +138,12 @@ ActiveRecord::Schema.define(:version => 20130801114520) do
   end
 
   add_index "authentications", ["user_id"], :name => "index_authentications_on_user_id"
+
+  create_table "categories", :force => true do |t|
+    t.integer  "activity_object_id"
+    t.datetime "created_at",         :null => false
+    t.datetime "updated_at",         :null => false
+  end
 
   create_table "comments", :force => true do |t|
     t.integer  "activity_object_id"
