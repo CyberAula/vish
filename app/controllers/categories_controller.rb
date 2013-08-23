2747854b55ad1a483fdb0624b99ebff37ae11970
Carolina Garcia <holacarol@gmail.com>
2013-08-23 21:07:18 +0200

    Change categories backend

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,3 +1,20 @@
 class CategoriesController < ApplicationController
   include SocialStream::Controllers::Objects
+
+  before_filter :add_item_to_category, :only => [:create, :update]
+
+  private
+
+  def add_item_to_category
+    if params[:category][:item_type].present?
+      @category.property_objects << params[:category][:item_type].constantize.find(params[:category][:item_id]).activity_object
+      params[:category].delete :item_type
+      params[:category].delete :item_id
+    end
+  end
+
+  def allowed_params
+    [:item_type, :item_id]
+  end
+
 end
