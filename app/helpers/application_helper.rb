d2033732b34b2dd6972aac49176633424d8d2d30
Enrique Barra <ebarra@dit.upm.es>
2013-09-02 13:50:02 +0200

    categories v6

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+	def categories_select(select_id, item)
+	  categories = subject_categories(current_subject, {:scope => :me, :limit => 0})
+	  subject_categories_array = categories.map { |category| [category.title, category.id] }.sort_by! {|cat| cat[0]} 
+	  categories_selection_array = [] 
+	  item.holder_categories.map { |category| categories_selection_array << category.id }
+	  select_tag(select_id, options_for_select(subject_categories_array, categories_selection_array), {:multiple => true, :style => 'width: 125px' })
+	end
+
 end
