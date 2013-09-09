b95bf6f95ec2937f5146ddee97bf1fa5b9e56aa7
Enrique Barra <enrique.barra.arias@gmail.com>
2013-09-09 14:32:22 +0200

    activity removed from search and categories in timeline empty

--- config/initializers/social_stream.rb
+++ config/initializers/social_stream.rb
@@ -37,7 +37,7 @@ SocialStream.setup do |config|
   # See SocialStream::Search for syntax
   # 
   config.quick_search_models = [:excursion, :user, :picture, :video, :audio, :swf, :officedoc, :document]
-  config.extended_search_models = [:excursion, :user, { :resource => [ :picture, :video, :audio, :swf, :officedoc, :document, :embed, :link ] }, { :activity2 => [ :comment ] } ]
+  config.extended_search_models = [:excursion, :user, { :resource => [ :picture, :video, :audio, :swf, :officedoc, :document, :embed, :link ] } ]
 
   # Cleditor controls. It is used in new message editor, for example
   # config.cleditor_controls = "bold italic underline strikethrough subscript superscript | size style | bullets | image link unlink"
