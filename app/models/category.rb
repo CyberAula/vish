61ffd23cf0d1bb8ef3293a29cee0e688dd987962
holacarol <carolgarcia@dit.upm.es>
2013-09-19 19:59:19 +0200

    Add categories validation

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,12 @@
 class Category < ActiveRecord::Base
   include SocialStream::Models::Object
+
+  validates_presence_of :title
+  #validates_uniqueness_of :title, :scope => :actor_id
+  validate :title_not_duplicated
+
+  private
+  def title_not_duplicated
+    errors.add(:base, "Title already exists") unless Category.all.map{ |category| category.id if(category.owner_id == self.owner_id && category.title == self.title) }.compact!.empty?
+  end
 end
