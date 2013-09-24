6d0b5d1ec52ca1f1850dfa707afe969b4e484d20
Enrique Barra <enrique.barra.arias@gmail.com>
2013-09-24 14:51:54 +0200

    new simple captcha

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130822182134) do
+ActiveRecord::Schema.define(:version => 20130924143351) do
 
   create_table "activities", :force => true do |t|
     t.integer  "activity_verb_id"
@@ -442,6 +442,15 @@ ActiveRecord::Schema.define(:version => 20130822182134) do
   add_index "shortened_urls", ["owner_id", "owner_type"], :name => "index_shortened_urls_on_owner_id_and_owner_type"
   add_index "shortened_urls", ["unique_key"], :name => "index_shortened_urls_on_unique_key", :unique => true
   add_index "shortened_urls", ["url"], :name => "index_shortened_urls_on_url"
+
+  create_table "simple_captcha_data", :force => true do |t|
+    t.string   "key",        :limit => 40
+    t.string   "value",      :limit => 6
+    t.datetime "created_at",               :null => false
+    t.datetime "updated_at",               :null => false
+  end
+
+  add_index "simple_captcha_data", ["key"], :name => "idx_key"
 
   create_table "sites", :force => true do |t|
     t.text     "config"
