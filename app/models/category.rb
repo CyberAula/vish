1fbfa67f52d19eb3b86c1c9c4d5f54666f5816e5
holacarol <carolgarcia@dit.upm.es>
2013-09-25 17:22:19 +0200

    Fix many category bugs

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,11 +2,10 @@ class Category < ActiveRecord::Base
   include SocialStream::Models::Object
 
   validates_presence_of :title
-  #validates_uniqueness_of :title, :scope => :actor_id
   validate :title_not_duplicated
 
   private
   def title_not_duplicated
-    errors.add(:base, "Title already exists") unless Category.all.map{ |category| category.id if(category.owner_id == self.owner_id && category.title == self.title) }.compact.blank?
+    errors.add(:title, "duplicated") unless Category.all.map{ |category| category.id if(category.owner_id == self.owner_id && category.title == self.title) }.compact.blank?
   end
 end
