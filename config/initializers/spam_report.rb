81feee2c3b1fcdc58374a62e26dfa0a35c58a33c
Enrique Barra <enrique.barra.arias@gmail.com>
2013-10-03 10:53:51 +0200

    spam report v1

---
+++ config/initializers/spam_report.rb
@@ -0,0 +1,5 @@
+
+#Configures the default from for the email sent
+Vish::Application.config.spam_report_from = "no-reply@vishub.org"
+Vish::Application.config.spam_report_recipient = "virtual.science.hub@gmail.com"
+  
