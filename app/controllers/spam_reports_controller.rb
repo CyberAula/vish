8e3d885bf095110bb28946344fb78232532646db
Enrique Barra <enrique.barra.arias@gmail.com>
2013-10-07 11:33:40 +0200

    email bug

--- app/controllers/spam_reports_controller.rb
+++ app/controllers/spam_reports_controller.rb
@@ -8,7 +8,7 @@ class SpamReportsController < ApplicationController
     @spam = SpamReport.new(:activity_object_id=> params[:activity_object_id], :reporter_user_id => current_subject ? current_subject.id : 0, :issue=> issue, :report_value=> params[:option])
     @spam.save!
     flash[:success] = t('spam.success')
-    SpamReportMailer.send_report(current_subject, params[:option], issue, params[:activity_object_id])
+    SpamReportMailer.send_report(current_subject, params[:option], issue, params[:activity_object_id]).deliver
     redirect_to request.referer
   end
 
