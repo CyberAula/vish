e8062028bf20aa928f06f5a590af6c68125b81ed
Enrique Barra <enrique.barra.arias@gmail.com>
2013-10-09 14:15:49 +0200

    categories drop down

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,11 +2,15 @@ module ApplicationHelper
 	def categories_select(select_id, item)
 	  categories = subject_categories(current_subject, {:scope => :me, :limit => 0})
 	  subject_categories_array = categories.map { |category| [category.title, category.id] }.sort_by! {|cat| cat[0]} 
-	  categories_selection_array = [] 
-	  item.holder_categories.map { |category| categories_selection_array << category.id }
+	  categories_selection_array = get_initial_categories(item)
 	  select_tag(select_id, options_for_select(subject_categories_array, categories_selection_array), {:multiple => true })
 	end
 
+	def get_initial_categories(item)	
+		categories_selection_array = []
+		item.holder_categories.map { |category| categories_selection_array << category.id }
+		categories_selection_array
+	end
 
 	def popular_excursions(number=10)
 		# We take visits and likes for now...
