d54121a0bfcc297734e9c15b9cb79747c0988f88
holacarol <carolgarcia@dit.upm.es>
2013-10-22 16:30:28 +0200

    Fix star icon in comments

--- config/initializers/social_stream.rb
+++ config/initializers/social_stream.rb
@@ -36,8 +36,8 @@ SocialStream.setup do |config|
   #
   # See SocialStream::Search for syntax
   # 
-  config.quick_search_models = [:excursion, :user, :picture, :video, :audio, :swf, :officedoc, :document, :embed, :link]
-  config.extended_search_models = [:excursion, :user, { :resource => [ :picture, :video, :audio, :swf, :officedoc, :document, :embed, :link ] } ]
+  config.quick_search_models = [:excursion, :user, :picture, :video, :audio, :swf, :officedoc, :document, :embed, :link, :event]
+  config.extended_search_models = [:excursion, :user, :event, { :resource => [ :picture, :video, :audio, :swf, :officedoc, :document, :embed, :link ] } ]
 
   # Cleditor controls. It is used in new message editor, for example
   # config.cleditor_controls = "bold italic underline strikethrough subscript superscript | size style | bullets | image link unlink"
