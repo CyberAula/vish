098b2eeb628b18fd7103567e45477b784dbfc52f
Enrique Barra <enrique.barra.arias@gmail.com>
2013-12-09 13:13:29 +0100

    link redirect

--- app/decorators/social_stream/linkser/links_controller_decorator.rb
+++ app/decorators/social_stream/linkser/links_controller_decorator.rb
@@ -3,5 +3,13 @@ LinksController.class_eval do
   def allowed_params
     [:url, :image, :callback, :width, :height, :callback_url, :loaded, :language, :age_min, :age_max, :tag_list=>[]]
   end
+
+  def create
+    super do |format|
+      format.json { render :json => resource }
+      format.js { render }
+      format.all {redirect_to user_path(current_user)}    
+  	end
+  end
   
 end
