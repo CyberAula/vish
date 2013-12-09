9e55c9acdcfdb2c91a5a3ee377adbfeac1cb141a
Enrique Barra <enrique.barra.arias@gmail.com>
2013-12-09 10:12:00 +0100

    change routes after create and after destroy, improve navigation2

--- app/decorators/social_stream/documents/documents_controller_decorator.rb
+++ app/decorators/social_stream/documents/documents_controller_decorator.rb
@@ -4,4 +4,28 @@ DocumentsController.class_eval do
     [:file, :language, :age_min, :age_max, :tag_list=>[]]
   end
   
+  def create
+    super do |format|
+      format.json { render :json => resource.to_json(helper: self), status: :created }
+      format.js
+      format.all {
+        if resource.new_record?
+          render action: :new
+        else
+          redirect_to resource
+        end
+      }
+    end
+  end
+
+  def destroy
+    super do |format|
+      format.html {
+        redirect_to current_user
+      }
+
+      format.js
+    end
+  end
+
 end
