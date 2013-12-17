fc162203864e5de839296286865aaa85b958ee30
Enrique Barra <enrique.barra.arias@gmail.com>
2013-12-17 10:20:49 +0100

    default to popularity field

---
+++ db/migrate/20131210115813_add_default_to_popularity_field.rb
@@ -0,0 +1,9 @@
+class AddDefaultToPopularityField < ActiveRecord::Migration
+  def up
+  	change_column :activity_objects, :popularity, :integer, :default => 0
+  end
+
+  def down
+  	change_column :activity_objects, :popularity, :integer, :default => nil
+  end
+end
