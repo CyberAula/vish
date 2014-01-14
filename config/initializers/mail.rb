c86f566473927865f88cabe3af3d05238433f7b8
Enrique Barra <enrique.barra.arias@gmail.com>
2014-01-14 10:02:16 +0100

    no mail for vishub-test

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -2,3 +2,6 @@
 ActionMailer::Base.default_url_options[:host] = 'vishub.org'
 ActionMailer::Base.smtp_settings[:enable_starttls_auto] = false
 
+if ENV['VISH_TEST']
+	ActionMailer::Base.smtp_settings[:perform_deliveries] = false
+end
