b537642334fa3dcdd87b1a1c7ad0387356a7beda
Enrique Barra <enrique.barra.arias@gmail.com>
2014-01-15 13:07:37 +0100

    comments sending button

--- app/assets/javascripts/comments.js
+++ app/assets/javascripts/comments.js
@@ -41,6 +41,7 @@ SocialStream.Comment = (function(SS, $, undefined){
 
   var initNewElements = function(){
     hideNewCommentElements();
+    initCommentButton();
 
     // show only the text fields for new comment
     // if there are any comment to the post
@@ -129,6 +130,19 @@ SocialStream.Comment = (function(SS, $, undefined){
     }
   };
 
+  // Prevent sending the same post several times
+  var initCommentButton = function() {
+    $('div.new_comment input[type="submit"]').on('click', loadingCommentButton);
+  };
+
+  var loadingCommentButton = function() {
+    $(this).button('loading');
+  };
+
+  var resetCommentButton = function(){
+    $('div.new_comment input[type="submit"]').button('reset');
+  };
+
   callback.register('index',
                     initNewElements,
                     newCommentAutoSize,
@@ -142,7 +156,8 @@ SocialStream.Comment = (function(SS, $, undefined){
                     hideNewActivityCommentElements,
                     newCommentAutoSize,
                     newCommentClick,
-                    newCommentLink);
+                    newCommentLink,
+                    resetCommentButton);
 
   return callback.extend({
     showAll: showAll
