e51a962afde69f82efdb8a41aa03d06320d9c968
Juan Carlos <juanka0087@hotmail.com>
2014-03-05 12:52:15 +0100

    catalog

--- app/controllers/catalogue_controller.rb
+++ app/controllers/catalogue_controller.rb
@@ -10,7 +10,7 @@ class CatalogueController < ApplicationController
 		end
 
 		respond_to do |format|
-      		format.all { render :layout => 'search' }
+      		format.all { render :layout => 'catalogue' }
     	end
 	end
 
@@ -20,7 +20,7 @@ class CatalogueController < ApplicationController
 		@excursions = search(@category, LIMIT_IN_SHOW)		
 
 		respond_to do |format|
-      		format.all { render :layout => 'search' }
+      		format.all { render :layout => 'catalogue' }
     	end
 	end
 
