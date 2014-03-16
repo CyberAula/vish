720510778b5d0b21af09b857d816a1f2408a05a0
Aldo Gordillo <agordillos@gmail.com>
2014-03-16 21:45:21 +0100

    LOEP API to receive evaluation data

--- app/controllers/loep/base_controller.rb
+++ app/controllers/loep/base_controller.rb
@@ -5,12 +5,12 @@ class Loep::BaseController < ActionController::Base
   ################
 
   def authenticate_app
-    if params["repository_name"].nil? or params["auth_token"].nil?
+    if params["app_name"].nil? or params["auth_token"].nil?
       render :json => ["Unauthorized"], :status => :unauthorized
       return
     end
 
-    if params["repository_name"]!="ViSH" or params["auth_token"] != Vish::Application.config.loep_token
+    if params["app_name"]!="ViSH" or params["auth_token"] != Vish::Application.config.loep_token
       render :json => ["Unauthorized"], :status => :unauthorized
       return
     end
--- app/controllers/loep/los_controller.rb
+++ app/controllers/loep/los_controller.rb
@@ -5,18 +5,30 @@ class Loep::LosController < Loep::BaseController
   def show
     respond_to do |format|
         format.any { 
-          render json: "Show Method: Success"
+          render json: "Ok"
         }
     end
   end
 
   # PUT /loep/los/:id
-  def update
+  def update  
     #New information about this LO is available on LOEP (e.g. a new evaluation and/or metric)
-    # @lo = Lo.find(params[:id])
+    @excursion = Excursion.find(params[:id])
+
+    eEvData = JSON(params["lo"])
+
+    # Interesting information
+    # eEvData["Completed Evaluations with LORI v1.5"]
+    # eEvData["Metric Score: LORI Arithmetic Mean"]
+    # eEvData["Metric Score: LORI Weighted Arithmetic Mean"]
+
+    # eEvData["LORI v1.5 item1"]
+    # ...
+    # eEvData["LORI v1.5 item9"]
+
     respond_to do |format|
         format.any { 
-          render json: "Update Method: Success"
+          render json: "Ok"
         }
     end
   end
