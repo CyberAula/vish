2a102c16b2921dcd485b133b7fc5a4e333953874
ebarra <ebarra@dit.upm.es>
2014-04-10 10:01:58 +0200

    disable spam report mailer until we add captcha. WARNING, UNDO

--- app/controllers/spam_reports_controller.rb
+++ app/controllers/spam_reports_controller.rb
@@ -8,7 +8,8 @@ class SpamReportsController < ApplicationController
     @spam = SpamReport.new(:activity_object_id=> params[:activity_object_id], :reporter_user_id => current_subject ? current_subject.id : 0, :issue=> issue, :report_value=> params[:option])
     @spam.save!
     flash[:success] = t('spam.success')
-    SpamReportMailer.send_report(current_subject, params[:option], issue, params[:activity_object_id]).deliver
+    #XXX TODO disable spam report mailer until we add captcha
+    #SpamReportMailer.send_report(current_subject, params[:option], issue, params[:activity_object_id]).deliver
     redirect_to request.referer
   end
 
