1a6dfdcb79e3a51569826454ca15a428645d0917
abenitoc <vermibeto@gmail.com>
2014-04-11 17:29:03 +0200

    Fixed spam report with captcha, not sure if alerts coming up though

--- app/controllers/spam_reports_controller.rb
+++ app/controllers/spam_reports_controller.rb
@@ -4,13 +4,19 @@ class SpamReportsController < ApplicationController
   def create
     #save reporter_user_id
     #redirect to excursion
+     if simple_captcha_valid?  
     issue = params[:comment_error] !="" ? params[:comment_error] : params[:comment_spam]
-    @spam = SpamReport.new(:activity_object_id=> params[:activity_object_id], :reporter_user_id => current_subject ? current_subject.id : 0, :issue=> issue, :report_value=> params[:option])
+    @spam = SpamReport.new(:activity_object_id=> params[:activity_object_id], :reporter_user_id => current_subject ? current_subject.id : 0, :issue=> issue, :report_value=> params[:option])    
     @spam.save!
     flash[:success] = t('spam.success')
     #XXX TODO disable spam report mailer until we add captcha
-    #SpamReportMailer.send_report(current_subject, params[:option], issue, params[:activity_object_id]).deliver
+    SpamReportMailer.send_report(current_subject, params[:option], issue, params[:activity_object_id]).deliver
     redirect_to request.referer
+     else
+        flash[:failure] = t('spam.failure')
+        redirect_to request.referer
+    end
+
   end
 
   
