c68ccc249e07a0e3694d1982f5d1596bb1f7b56d
abenitoc <vermibeto@gmail.com>
2014-04-11 14:32:34 +0200

    Added popularity funcionality in searches

--- lib/social_stream/base/thinking-sphinx.rb
+++ lib/social_stream/base/thinking-sphinx.rb
@@ -10,6 +10,7 @@ module SocialStream
 
             has activity_object.popularity, :as => :popularity, :sortable => true
             has created_at
+            has updated_at
             has activity_object.author_actions(:actor_id), :as => :author_id
             has activity_object.owner_actions(:actor_id),  :as => :owner_id
             has activity_object.activity_object_audiences(:relation_id), :as => :relation_ids
