5cf322387a3a64e9114b78143d66e65642449a0b
abenitoc <vermibeto@gmail.com>
2014-04-21 14:24:09 +0200

    Fix flashy

--- app/controllers/spam_reports_controller.rb
+++ app/controllers/spam_reports_controller.rb
@@ -8,12 +8,12 @@ class SpamReportsController < ApplicationController
     issue = params[:comment_error] !="" ? params[:comment_error] : params[:comment_spam]
     @spam = SpamReport.new(:activity_object_id=> params[:activity_object_id], :reporter_user_id => current_subject ? current_subject.id : 0, :issue=> issue, :report_value=> params[:option])    
     @spam.save!
-    flash[:success] = t('spam.success')
+    flash.now[:success] = t('spam.success')
     #XXX TODO disable spam report mailer until we add captcha
     SpamReportMailer.send_report(current_subject, params[:option], issue, params[:activity_object_id]).deliver
     redirect_to request.referer
      else
-        flash[:failure] = t('spam.failure')
+        flash.now[:failure] = t('spam.failure')
         redirect_to request.referer
     end
 
