fe9c82175a4fd7894735730059dc6e7eff559723
abenitoc <vermibeto@gmail.com>
2014-04-25 15:31:15 +0200

    Delete mve from migrations

--- db/migrate/20140424132322_add_mve.rb
+++
@@ -1,8 +0,0 @@
-class AddMve < ActiveRecord::Migration
-  def up
-  	add_column :activity_objects, :mve, :integer
-  end
-
-  def down
-  end
-end
--- db/migrate/20140424132545_changing_default_mve.rb
+++
@@ -1,9 +0,0 @@
-class ChangingDefaultMve < ActiveRecord::Migration
- def up
-  	change_column :activity_objects, :mve, :integer, :default => 0
-  end
-
-  def down
-  	change_column :activity_objects, :mve, :integer, :default => nil
-  end
-end
