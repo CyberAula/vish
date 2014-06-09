8c2ba47f03296b1f38d08052cf1c6082299f3f08
ebarra <ebarra@dit.upm.es>
2014-06-09 13:46:54 +0200

    migrations fix 2

--- db/migrate/20120112144748_create_social_stream.rb
+++ db/migrate/20120112144748_create_social_stream.rb
@@ -200,14 +200,30 @@ class CreateSocialStream < ActiveRecord::Migration
     create_table "users", :force => true do |t|
       t.string :encrypted_password, :null => false, :default => "", :limit => 128
       t.string :password_salt
-      
-      t.recoverable
-      t.rememberable
-      t.trackable
+
+      #t.recoverable
+      #t.rememberable
+      #t.trackable
+
+      ## Recoverable
+      t.string   :reset_password_token
+      t.datetime :reset_password_sent_at
+
+      ## Rememberable
+      t.datetime :remember_created_at
+
+      ## Trackable
+      t.integer  :sign_in_count, :default => 0
+      t.datetime :current_sign_in_at
+      t.datetime :last_sign_in_at
+      t.string   :current_sign_in_ip
+      t.string   :last_sign_in_ip
       
       # t.confirmable
       # t.lockable :lock_strategy => :failed_attempts, :unlock_strategy => :both
-      t.token_authenticatable
+      #t.token_authenticatable
+      # Token authenticatable
+      t.string :authentication_token
       
       t.timestamps
       t.integer  "actor_id"
