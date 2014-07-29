cc8c3c68de8f12b9dffb039bea2af327f82bf83d
Alvaro Sanchez Bravo <a.sanbravo@gmail.com>
2014-07-29 11:32:50 +0200

    Short answer with selfA = true and selfA = false implemented. Now exporting those questions to IMS QTI

--- app/models/moodlexml.rb
+++ app/models/moodlexml.rb
@@ -43,6 +43,15 @@ class MOODLEQUIZXML
             zos.put_next_entry(fileName + ".xml")
             #Zero-width space <200b> erased from target in moodlequizmc
             zos.print moodlequizmc.target!().gsub("\u{200B}","" )
+
+          when "openAnswer"
+            if qjson["selfA"] == true
+              moodlequizoa = MOODLEQUIZXML.generate_MoodleQUIZSA(qjson)
+            else
+              moodlequizoa = MOODLEQUIZXML.generate_MoodleQUIZLA(qjson)
+            end
+              zos.put_next_entry(fileName + ".xml")
+              zos.print moodlequizoa.target!().gsub("\u{200B}","" )
           else
       end
     end   
@@ -94,55 +103,114 @@ class MOODLEQUIZXML
     return myxml;
   end
 
-
-  def self.generate_MoodleQUIZTF(qjson)
-    myxml = ::Builder::XmlMarkup.new(:indent => 2)
-    myxml.instruct! :xml, :version => "1.0", :encoding => "UTF-8"
-
-
-    nChoices = qjson["choices"].size
-
-    if qjson["extras"]["multipleAnswer"] == false 
-      card = "true"
-    else
-      card = "false"
-    end 
-
-    myxml.quiz do
+  def self.generate_MoodleQUIZTF(qjson,index)  
+    myxml.assessmentItem("xmlns"=>"http://www.imsglobal.org/xsd/imsqti_v2p1", "xmlns:xsi"=>"http://www.w3.org/2001/XMLSchema-instance", "xsi:schemaLocation"=>"http://www.imsglobal.org/xsd/imsqti_v2p1  http://www.imsglobal.org/xsd/qti/qtiv2p1/imsqti_v2p1p1.xsd","identifier"=>"choiceMultiple", "title"=> title_identifier, "timeDependent"=>"false", "adaptive" => "false") do
       
-      myxml.question("type" => "category") do
-        myxml.category do
-          myxml.text do
-            myxml.text!("Moodle QUIZ XML export")
-          end
-        end
-      end
-
-      myxml.question("type" => "multichoice") do
-        myxml.name do
-          myxml.text do
-            myxml.text!(qjson["question"]["value"])
-          end
-        end
-        for i in 0..((nChoices)-1)
-          if qjson["choices"][i]["answer"] == true
-            mappedV = "100"
+      myxml.responseDeclaration("identifier"=>"RESPONSE", "cardinality" => "single", "baseType" => "identifier") do
+        
+        myxml.correctResponse() do
+          if qjson["choices"][index]["answer"] == true 
+            myxml.value("A0")
           else
-            mappedV = "0"
-          end
-          myxml.answer("fraction" => mappedV) do
-            myxml.text(qjson["choices"][i]["value"])
+            myxml.value("A1")
           end
         end
-          
-        myxml.shuffleanswers("1")
-        myxml.single(card)
+        
+        myxml.mapping("lowerBound" => "-1", "upperBound"=>"1", "defaultValue"=>"0") do
+          if qjson["choices"][index]["answer"] == true
+            myxml.mapEntry("mapKey"=>"A0", "mappedValue"=> 1)
+            myxml.mapEntry("mapKey"=> "A1", "mappedValue"=> -1)
+          else
+            myxml.mapEntry("mapKey"=>"A0", "mappedValue"=> -1)
+            myxml.mapEntry("mapKey"=> "A1", "mappedValue"=> 1)
+          end             
+        end
+
       end
 
+      myxml.outcomeDeclaration("identifier"=>"SCORE", "cardinality"=>"single", "baseType"=>"float") do
+      end
+
+      myxml.itemBody() do
+        myxml.choiceInteraction("responseIdentifier"=>"RESPONSE", "shuffle" => "false", "maxChoices" => "1", "minChoices"=>"0") do
+          myxml.prompt(qjson["question"]["value"]  + ": " + qjson["choices"][index]["value"])
+          myxml.simpleChoice("True","identifier"=>"A0")
+          myxml.simpleChoice("False","identifier"=>"A1") 
+        end
+      end
+
+      myxml.responseProcessing()
     end
 
     return myxml;
   end
+
+def self.generate_MoodleQUIZSA(qjson)
+  myxml = ::Builder::XmlMarkup.new(:indent => 2)
+  myxml.instruct! :xml, :version => "1.0", :encoding => "UTF-8"
+
+  myxml.quiz do  
+      myxml.question("type" => "category") do
+        myxml.category do
+          myxml.text("Moodle QUIZ XML export")
+        end
+      end
+
+      myxml.question("type" => "shortanswer") do
+        myxml.name do
+          myxml.text(((qjson["question"]["value"]).to_s).lstrip.chop)  
+        end
+        myxml.questiontext do
+          myxml.text(((qjson["question"]["value"]).to_s).lstrip.chop)  
+        end
+        myxml.answer("fraction" => "100") do
+            myxml.text(((qjson["answer"]["value"]).to_s).lstrip.chop)
+          end
+      end
+    end
+    return myxml;
+end
+
+def self.generate_MoodleQUIZLA(qjson)
+ myxml = ::Builder::XmlMarkup.new(:indent => 2)
+  myxml.instruct! :xml, :version => "1.0", :encoding => "UTF-8"
+
+  myxml.quiz do  
+      myxml.question("type" => "category") do
+        myxml.category do
+          myxml.text("Moodle QUIZ XML export")
+        end
+      end
+
+      myxml.question("type" => "essay") do
+        myxml.name do
+          myxml.text(((qjson["question"]["value"]).to_s).lstrip.chop)  
+        end
+        myxml.questiontext do
+          myxml.text(((qjson["question"]["value"]).to_s).lstrip.chop)  
+        end
+        myxml.answer("fraction" => "0") do
+            myxml.text
+          end
+      end
+    end
+    return myxml;
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
