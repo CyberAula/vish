4df34f3ac7c1e676a4afce6792c9314bd8f905a3
Aldo Gordillo <agordillos@gmail.com>
2014-07-30 16:05:08 +0200

    Infer language from client on user registration

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,6 +2,12 @@ class RegistrationsController < Devise::RegistrationsController
     
     def create
       if simple_captcha_valid?
+
+        #Infer user language from client information
+        if !I18n.locale.nil? and !params[:user].nil? and Vish::Application.config.platformLanCodes.include? I18n.locale.to_s
+          params[:user][:language] = I18n.locale.to_s
+        end
+
         super
       else
         build_resource
--- app/models/excursion.rb
+++ app/models/excursion.rb
@@ -634,7 +634,8 @@ class Excursion < ActiveRecord::Base
   def self.getLOMLoLanguage(language, _LOMschema)
     #List of language codes according to ISO-639:1988
     # lanCodes = ["aa","ab","af","am","ar","as","ay","az","ba","be","bg","bh","bi","bn","bo","br","ca","co","cs","cy","da","de","dz","el","en","eo","es","et","eu","fa","fi","fj","fo","fr","fy","ga","gd","gl","gn","gu","gv","ha","he","hi","hr","hu","hy","ia","id","ie","ik","is","it","iu","ja","jw","ka","kk","kl","km","kn","ko","ks","ku","kw","ky","la","lb","ln","lo","lt","lv","mg","mi","mk","ml","mn","mo","mr","ms","mt","my","na","ne","nl","no","oc","om","or","pa","pl","ps","pt","qu","rm","rn","ro","ru","rw","sa","sd","se","sg","sh","si","sk","sl","sm","sn","so","sq","sr","ss","st","su","sv","sw","ta","te","tg","th","ti","tk","tl","tn","to","tr","ts","tt","tw","ug","uk","ur","uz","vi","vo","wo","xh","yi","yo","za","zh","zu"]
-    lanCodesMin = ["de","en","es","fr","it","pt","hu"]
+    lanCodesMin = Vish::Application.config.platformLanCodes
+    lanCodesMin.concat(["it","pt"])
 
     case _LOMschema
     when "ODS"
