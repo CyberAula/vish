11fbd7099c14e5e1cabf6bf3cd5a37df710391e8
Aldo Gordillo <agordillos@gmail.com>
2014-08-07 19:33:52 +0200

    Several fixes for the catalogue

--- app/controllers/catalogue_controller.rb
+++ app/controllers/catalogue_controller.rb
@@ -1,41 +1,30 @@
 class CatalogueController < ApplicationController
 
-	def index
-		@default_categories = Hash.new
-		for category in Vish::Application.config.default_categories
-			@default_categories[category] = getCategoryResources(category,7)
-		end
+  def index
+    @default_categories = view_context.getDefaultCategories
 
-		respond_to do |format|			
-      		format.html { 
-      			if request.xhr?
-      				  if params[:category] 
-		                @excursions = getCategoryResources(params[:category])
-		                render :partial => 'catalogue/show'
-		              else               
-		                render :partial => "catalogue/index", :locals =>{ :is_home=> params[:is_home]}
-		              end
-      			else
-      				render :layout => 'catalogue' 
-      			end
-      		}    		
-    	end
-	end
+    respond_to do |format|
+      format.html {
+        if request.xhr?
+          if params[:category]
+            @excursions = view_context.getCategoryResources(params[:category])
+            render :partial => 'catalogue/show'
+          else
+            render :partial => "catalogue/index", :locals =>{ :is_home=> params[:is_home]}
+          end
+        else
+          render :layout => 'catalogue' 
+        end
+      }    		
+    end
+  end
 
-	def show
-		@category = params[:category]
-		@excursions = getCategoryResources(@category)		
-		respond_to do |format|
-			format.all { render :layout => 'catalogue' }    		
-    	end
-	end
-
-
-	private
-
-	def getCategoryResources(category,limit=100)
-		keywords = Vish::Application.config.catalogue[category]
-		RecommenderSystem.search({:keywords=>keywords, :n=>limit, :models => [Excursion], :order => 'ranking DESC', :qualityThreshold=>5})
-	end
+  def show
+    @category = params[:category]
+    @excursions = view_context.getCategoryResources(@category)	
+    respond_to do |format|
+      format.all { render :layout => 'catalogue' }
+    end
+  end
 
 end
