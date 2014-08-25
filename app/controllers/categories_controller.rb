629ae8fd31fc2d8932cdf7d81ce7c3e1e78fab65
vermibeto@gmail.com <vermibeto@gmail.com>
2014-08-25 23:40:28 +0200

    Modified categories stuff

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -47,20 +47,29 @@ class CategoriesController < ApplicationController
           actorId = Actor.normalize_id(current_subject)
           c.author_id = actorId
           c.owner_id = actorId
-          binding.pry
           c.save!
           existing_categories_ids.push(c.id)
         end
       end
 
-      #Categorize the resource with the categories stored in existing_categories_ids
       existing_categories_ids.uniq!
+
+      object_id = ActivityObject.find(params[:activity_object_id])
+      subject_categories = Category.authored_by(current_subject)
+      
+      subject_categories.each do |delet|
+        delet.property_objects.delete(object_id)
+      end
+
       categories_to_categorize = Category.find(existing_categories_ids)
       categories_to_categorize.each do |categoryToCategorize|
-        # categoryToCategorize.property_objects << item
+         categoryToCategorize.property_objects << object_id
+         categoryToCategorize.property_objects.uniq!
       end
-    end
 
+
+    end
+    render :json => { :success => true }
     #Category.all
     #Check in categories which categories are new and create them, when done, get an array with all ids
     #apply the categories to an item
