867fda08bf8b69074eaf8b2b6789fc21e59b82e0
ebarra <ebarra@dit.upm.es>
2014-09-26 13:23:22 +0200

    polymorphic workshop_activity

--- app/models/wa_contributions_gallery.rb
+++ app/models/wa_contributions_gallery.rb
@@ -1,5 +1,5 @@
 class WaContributionsGallery < ActiveRecord::Base
-	belongs_to :workshop_activity
+	has_one :workshop_activity, as: :wa_activity 
 	has_and_belongs_to_many :wa_assignment
 	has_many :contributions, :through => :wa_assignment
 
