5343851c425a081e9142cf0732a75960bcfb0d32
vermibeto@gmail.com <vermibeto@gmail.com>
2014-10-04 20:50:24 +0200

    Draggable done now need of droppable

--- db/migrate/20141004180159_category_order.rb
+++ db/migrate/20141004180159_category_order.rb
@@ -1,9 +1,9 @@
 class CategoryOrder < ActiveRecord::Migration
   def up
-  	add_column :user, :category_order, :integer
+  	add_column :actors, :category_order, :integer
   end
 
   def down
-  	remove_column :user, :category_order
+  	remove_column :actors, :category_order
   end
 end
