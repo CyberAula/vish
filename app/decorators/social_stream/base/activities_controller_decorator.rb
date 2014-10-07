5b6f2b1dc2f14c4cbdd95edbf1509668ee3e27c9
Aldo Gordillo <agordillos@gmail.com>
2014-10-07 11:06:20 +0200

    Filter AOs with private scope in the timeline

--- app/decorators/social_stream/base/activities_controller_decorator.rb
+++ app/decorators/social_stream/base/activities_controller_decorator.rb
@@ -1,11 +1,7 @@
 ActivitiesController.class_eval do
 
   def index
-    # should be activities.page(params[:page], :count => { :select => 'activity.id', :distinct => true }) but it is not working in Rails 3.0.3 
-    @activities =
-      Activity.timeline(current_section,
-                        current_subject).
-               page(params[:page])
+    @activities = Activity.timeline(current_section,current_subject).page(params[:page])
 
     respond_to do |format|
       format.html {
