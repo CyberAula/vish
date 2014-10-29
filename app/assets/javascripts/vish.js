f8037a328979561eef2a85b2d73a2f96a269410a
crispinh2o <crispinh2o@hotmail.com>
2014-10-29 19:20:05 +0100

    autosize run

--- app/assets/javascripts/vish.js
+++ app/assets/javascripts/vish.js
@@ -14,3 +14,7 @@ $("[rel=dropdown]").tooltip({ placement: 'bottom'});
 $(document).ready(function(){
 $("select").select2();
 });
+
+$(document).ready(function(){
+    $('textarea').autosize();    
+});
