21a84013371de707a9faf42a495daec022cbc614
Aldo Gordillo <agordillos@gmail.com>
2014-11-04 17:42:32 +0100

    Wa Resources Gallery activity

--- app/models/wa_contributions_gallery.rb
+++ app/models/wa_contributions_gallery.rb
@@ -5,7 +5,7 @@ class WaContributionsGallery < ActiveRecord::Base
   has_and_belongs_to_many :wa_assignments
   has_many :contributions, :through => :wa_assignments
 
-	validate :has_wa_assignments
+  validate :has_wa_assignments
   def has_wa_assignments
     if self.wa_assignments.blank?
       errors.add(:contributions_gallery, "A contributions gallery should include at least 1 assignment")
