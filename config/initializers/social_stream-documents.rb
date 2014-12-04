b68567e0f6e2d21f1d4b407d5f283d7222e9d453
ebarra <ebarra@dit.upm.es>
2014-12-04 12:56:09 +0100

    audio and video formats by configuracion

--- config/initializers/social_stream-documents.rb
+++ config/initializers/social_stream-documents.rb
@@ -7,34 +7,29 @@ SocialStream::Documents.setup do |config|
     :"500" => ["500>"]
   }
 
-  #Configure audio thumbnails
-  config.audio_styles = {
-    webma: {
-      format: 'webm',
-      processors: [:ffmpeg]
-    },
-    mp3: {
-      format: 'mp3',
-      processors: [:ffmpeg]
-    },
-    wav: {
-      format: 'wav',
-      processors: [:ffmpeg]
-    }
-  }
+  config.audio_styles = { }
+  config.audio_processors = []
+  
+  if Vish::Application.config.APP_CONFIG["services"].include? "MediaConversion"
+    #Configure audio thumbnails
+    config.audio_processors = [:ffmpeg]
+    config.audio_styles[:webma] = {format: 'webm', processors: [:ffmpeg] }
+    config.audio_styles[:mp3] = {format: 'mp3', processors: [:ffmpeg] }
+    config.audio_styles[:wav] = {format: 'wav', processors: [:ffmpeg] }   
+
+  end
 
   #Configure video thumbnails
-  config.video_styles = {
-    :webm => {  :format => 'webm' },
-    :flv  => {  :format => 'flv',
-                :convert_options => { :output => {:ar =>'22050'}}
-    }, 
-    :mp4  => {  :format => 'mp4',
-                :convert_options => { :output => {:vcodec =>'libx264', :acodec =>"aac", :strict => "-2"}},
-                :streaming => true
-             },
-    :"170x127#" => { :geometry => "170x127#", :format => 'png', :time => 4 }
-  }
+  config.video_styles = { }
+  config.video_processors = []
+
+  if Vish::Application.config.APP_CONFIG["services"].include? "MediaConversion"
+      config.video_processors = [:ffmpeg, :qtfaststart]
+      config.video_styles[:"170x127#"] = {  :geometry => "170x127#", :format => 'png', :time => 4 }
+      config.video_styles[:webm] = {  :format => 'webm' }
+      config.video_styles[:flv] = {  :format => 'flv', :convert_options => { :output => {:ar =>'22050'}}}
+      config.video_styles[:mp4] = {  :format => 'mp4', :convert_options => { :output => {:vcodec =>'libx264', :acodec =>"aac", :strict => "-2"}}, :streaming => true }   
+  end
 
 
   #  List of mime types that have an icon defined
@@ -65,8 +60,16 @@ SocialStream::Documents.setup do |config|
     ]
   }
 
-  config.subtype_classes_mime_types[:video] = [:flv, :webm, :mp4, :mpeg, :mov, :wmv, :m4v, :ogv, :gpp, :gpp2]
-  config.subtype_classes_mime_types[:audio] = [:aac, :gppa, :gpa, :wav, :ogg, :webma, :mp3]
+  config.subtype_classes_mime_types[:video] = [:flv, :webm, :mp4, :ogv]
+  if Vish::Application.config.APP_CONFIG["services"].include? "MediaConversion"
+    config.subtype_classes_mime_types[:video].push(:mpeg, :mov, :wmv, :m4v, :gpp, :gpp2)
+  end
+
+  config.subtype_classes_mime_types[:audio] = [:wav, :ogg, :webma, :mp3, :m4a]
+  if Vish::Application.config.APP_CONFIG["services"].include? "MediaConversion"
+    config.subtype_classes_mime_types[:audio].push(:aac, :gppa, :gpa)
+  end
+
   config.subtype_classes_mime_types[:swf] = [:swf]
   config.subtype_classes_mime_types[:zipfile] = [:zipfile]
   config.subtype_classes_mime_types[:officedoc]= [:odt, :odp, :ods, :doc, :ppt, :xls, :rtf, :pdf]
