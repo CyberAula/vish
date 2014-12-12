3bc16ead079c3963c2cbc75e6ec8be733e826f9b
Aldo Gordillo <agordillos@gmail.com>
2014-12-12 11:52:19 +0100

    Read PDF files using the Web browser PDF Reader when is possible

--- app/models/officedoc.rb
+++ app/models/officedoc.rb
@@ -1,17 +1,19 @@
 class Officedoc < Document  
-  has_attached_file :file, 
+  has_attached_file :file,
                     :url => '/:class/:id.:extension',
                     :path => ':rails_root/documents/:class/:id_partition/:filename.:extension'
-  
-  define_index do
-    activity_object_index
 
-    indexes file_file_name, :as => :file_name
-  end 
-              
   # Thumbnail file
   def thumb(size, helper)
-      "#{ size.to_s }/audio.png"
+    "#{ size.to_s }/officedoc.png"
   end
-  
+
+  def source_full_url
+    Vish::Application.config.full_domain + self.file.url
+  end
+
+  def google_doc_url
+    "http://docs.google.com/viewer?url=" + source_full_url + "&embedded=true"
+  end
+
 end
