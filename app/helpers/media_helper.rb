770bbe342245c11c14f4a92aff9cc392f9e4cca5
agordillo <agordillos@gmail.com>
2015-01-29 11:54:24 +0100

    Fix embed on videos and audios

--- app/helpers/media_helper.rb
+++ app/helpers/media_helper.rb
@@ -7,7 +7,6 @@ module MediaHelper
 		all_data_url += printDataUrlIfPresent(media, :webm) + " "
 		all_data_url += printDataUrlIfPresent(media, :flv) + " "
 		all_data_url += printDataUrlIfPresent(media, :mp4) + " "
-		all_data_url += printDataUrlIfPresent(media, :webm) + " "
 		all_data_url += printDataUrlPosterIfPresent(media, :png, '170x127#') + " "
 	end
 
@@ -36,37 +35,33 @@ module MediaHelper
 	end
 
 	#method to print data-url-webm in the media player
-	#only if the media has been converted or it is webm format
+	#only if the media has been converted or it is the format
 	def printDataUrlIfPresent(media, format)
 		if available_services.include?("MediaConversion") || media.format == format
-			string = "data-url-"+format.to_s+"="
-			string += polymorphic_path(media, :format => format)
+			string = "data-url-" + format.to_s + "=" + Vish::Application.config.full_domain + polymorphic_path(media, :format => format)
 			return raw string
 		else
 			return ""
-		end	
+		end
 	end
-
 
 	#method to print data-url-webm in the media player
-	#only if the media has been converted or it is webm format
+	#only if the media has been converted or it is the format
 	def printDataUrlPosterIfPresent(media, format, style)
 		if available_services.include?("MediaConversion")
-			string = "data-url-poster="
-			string += polymorphic_path(media, :format => format, :style => style)
+			string = "data-url-poster=" + Vish::Application.config.full_domain + polymorphic_path(media, :format => format, :style => style)
 			return raw string
 		else
 			return ""
 		end	
 	end
 
-
 	#method to print source in the media tag
-	#only if the media has been converted or it is webm format
+	#only if the media has been converted or it is the format
 	def printSourceIfPresent(media, format)
 		if available_services.include?("MediaConversion") || media.format == format
-			string = "<source src="
-			string += "'" + polymorphic_path(media, :format => format) + "'"
+			string = "    " + "<source src="
+			string += "'" + Vish::Application.config.full_domain + polymorphic_path(media, :format => format) + "'"
 			string += " type='"+Mime::Type.lookup_by_extension(format).to_s+"'>\n "
 			return raw string
 		else
@@ -74,13 +69,11 @@ module MediaHelper
 		end	
 	end
 
-
 	#method to print data-url-webm in the media player
-	#only if the media has been converted or it is webm format
+	#only if the media has been converted or it is the format
 	def printSourcePosterIfPresent(media, format, style)
 		if available_services.include?("MediaConversion")
-			string = "poster="
-			string += "'" + polymorphic_path(media, :format => format, :style => style) + "'"
+			string = "poster='" + Vish::Application.config.full_domain + polymorphic_path(media, :format => format, :style => style) + "'"
 			return raw string
 		else
 			return ""
