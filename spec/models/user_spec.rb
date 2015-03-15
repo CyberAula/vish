72ccaa77eebed0ad7ac8751711ab68f378207e54
vermibeto@gmail.com <vermibeto@gmail.com>
2015-03-15 18:04:01 +0100

    Test(4) - Added basic models to rspec, just realised database can be filled by factories for unit test or by a rake task for a more specific filling

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+
+describe User do
+
+it "user is created"
+it "user is also an activity_object"
+it "user has an actor associated"
+it "user can create excursion"
+it "user can create event"
+it "user can create Document"
+it "user can create Category"
+it "user can follow other user"
+it "user can be followed by other user"
+it "user can be gone from platform"
+
+end
