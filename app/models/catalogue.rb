d7aad2f65e7c482cbfcc65ff6fd390dc149fd2d8
vermibeto@gmail.com <vermibeto@gmail.com>
2015-03-19 17:26:02 +0100

    added

---
+++ app/models/catalogue.rb
@@ -0,0 +1,28 @@
+# encoding: utf-8
+
+###############
+# Catalogue
+###############
+
+class Catalogue
+
+  def self.getCategoryResources(category,limit=100)
+    if Vish::Application.config.catalogue['mode'] == "matchtag"
+      #Mode matchtag
+      RecommenderSystem.search({:category_id=>category, :n=>limit, :models => VishConfig.getCatalogueModels({:return_instances => true}), :order => 'ranking DESC', :qualityThreshold => Vish::Application.config.catalogue["qualityThreshold"]})
+    else
+      #Mode matchany
+      keywords = Vish::Application.config.catalogue["category_keywords"][category]
+      RecommenderSystem.search({:keywords=>keywords, :n=>limit, :models => VishConfig.getCatalogueModels({:return_instances => true}), :order => 'ranking DESC', :qualityThreshold => Vish::Application.config.catalogue["qualityThreshold"]})
+    end
+  end
+
+  def self.getDefaultCategories
+    default_categories = Hash.new
+    for category in Vish::Application.config.catalogue["default_categories"]
+      default_categories[category] = Catalogue.getCategoryResources(category,7)
+    end
+    default_categories
+  end
+
+end
--- config/application.rb
+++ config/application.rb
@@ -99,7 +99,7 @@ module Vish
     end
 
     if config.tagsSettings["maxLength"].blank?
-        config.tagsSettings["maxLength"] = 40
+        config.tagsSettings["maxLength"] = 20
     end
 
     if config.tagsSettings["maxTags"].blank?
@@ -107,18 +107,24 @@ module Vish
     end
 
     if config.tagsSettings["triggerKeys"].blank?
-        config.tagsSettings["triggerKeys"] = ['enter', 'comma', 'tab']
+        config.tagsSettings["triggerKeys"] = ['enter', 'comma', 'tab', 'space']
     end
 
-    #Catalogue mode
+    #Catalogue
     if config.APP_CONFIG['catalogue'].blank?
         config.catalogue = {} 
     else
         config.catalogue = config.APP_CONFIG['catalogue']
     end
 
+    #Catalogue: Mode
     if config.catalogue['mode'].blank?
         config.catalogue['mode'] = "matchany"
+    end
+
+    #Catalogue: Quality threshold
+    unless config.catalogue["qualityThreshold"].is_a? Numeric
+        config.catalogue["qualityThreshold"] = nil
     end
 
   end
