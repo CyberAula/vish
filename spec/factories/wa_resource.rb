d611c1d157967d0b2d6dc7cfcf34cb8a1d3f7762
vermibeto@gmail.com <vermibeto@gmail.com>
2015-04-08 19:24:43 +0200

    Test(13) - More files

--- spec/factories/wa_resource.rb
+++ spec/factories/wa_resource.rb
@@ -1,1 +1,7 @@
-#TODO
+include ActionDispatch::TestProcess
+
+Factory.define :waresource do |u|
+	u.sequence(:title) { |n| "name#{ n }" }
+ 	u.sequence(:description) { |n| "desc#{ n }" }
+ 	u.workshop {|wshop| wshop.association(:workshop) }
+end
--- spec/factories/workshop_activity.rb
+++ spec/factories/workshop_activity.rb
@@ -1,1 +1,7 @@
-#TODO
+include ActionDispatch::TestProcess
+
+Factory.define :workshopactivity do |u|
+	u.sequence(:title) { |n| "name#{ n }" }
+ 	u.sequence(:description) { |n| "desc#{ n }" }
+ 	u.workshop {|wshop| wshop.association(:workshop) }
+end
