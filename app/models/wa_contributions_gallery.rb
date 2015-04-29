820d83e3ae43bcb616ed0051c5d96fb3a4905cef
pmverdugo <pm.verdugo@alumnos.upm.es>
2015-04-29 12:23:59 +0200

    locale tag fix

--- app/models/wa_contributions_gallery.rb
+++ app/models/wa_contributions_gallery.rb
@@ -8,7 +8,7 @@ class WaContributionsGallery < ActiveRecord::Base
   validate :has_wa_assignments
   def has_wa_assignments
     if self.wa_assignments.blank?
-      errors.add(:contributions_gallery, "A contributions gallery should include at least 1 assignment")
+      errors.add(:contributions_gallery, t('workshop.activities.contributions_gallery.error'))
     else
       true
     end
