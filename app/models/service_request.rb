c64dfd7e9108fed38a8c65dbeb56bf63a60f27f6
Lourdesmarco <lourdesmarco.design@gmail.com>
2015-08-04 12:40:27 +0200

    admin modal request details_finishing styles

--- app/models/service_request.rb
+++ app/models/service_request.rb
@@ -1,7 +1,7 @@
 class ServiceRequest < ActiveRecord::Base
   belongs_to :owner, class_name: "Actor"
 
-  has_attached_file :attachment, 
+  has_attached_file :attachment,
                     :url => '/service_requests/:id/attachment',
                     :path => ':rails_root/documents/:class/attachments/:id_partition/:filename.:extension'
   validates_attachment_size :attachment, :in => 0.megabytes..8.megabytes, :message => 'Attachment file size is too big'
@@ -21,6 +21,15 @@ class ServiceRequest < ActiveRecord::Base
 
   def afterAccept
     #Override this method on the specific ServiceRequest
+  end
+
+
+  def readable_type
+    if type=="ServiceRequest::PrivateStudentGroup"
+      "private_student"
+    else
+      "default"
+    end
   end
 
 end
