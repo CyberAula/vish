93ae6644e15c19db5dcc1a99ad4c7d809cd13e61
abenitoc <vermibeto@gmail.com>
2015-08-16 22:05:31 +0200

    changed schemas

--- db/schema.rb
+++ db/schema.rb
@@ -11,8 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20150730111321) do
-
+ActiveRecord::Schema.define(:version => 20150804121624) do
 
   create_table "activities", :force => true do |t|
     t.integer  "activity_verb_id"
@@ -275,6 +274,9 @@ ActiveRecord::Schema.define(:version => 20150730111321) do
     t.string   "attachment_content_type"
     t.integer  "attachment_file_size"
     t.datetime "attachment_updated_at"
+    t.boolean  "allow_download",          :default => true
+    t.boolean  "allow_comment",           :default => true
+    t.boolean  "allow_clone",             :default => true
   end
 
   create_table "groups", :force => true do |t|
