b4193ccfc154b410a7937f36538b11ff2b716beb
ebarra <ebarra@dit.upm.es>
2015-09-22 16:44:02 +0200

    SEO improvements v0.1

--- app/helpers/meta_helper.rb
+++ app/helpers/meta_helper.rb
@@ -1,3 +1,19 @@
 module MetaHelper
-	
+
+	#helpers for user profile
+	def profile_title user, tab
+		if tab.nil?
+			user.name + " - " + t("site.meta_title.profile")
+		else
+			user.name + " - " + t("search.models."+tab.singularize)
+		end
+	end	
+
+	def profile_desc user, tab
+		if tab.nil?
+			t("site.meta_desc.profile") + user.name + ". " + t("site.meta_desc.profile2")
+		else
+			t("site.meta_desc.tab_"+tab) + user.name
+		end
+	end	
 end
