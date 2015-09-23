17ee38e2f28e1707d6ee59e483b30713fa524da1
ebarra <ebarra@dit.upm.es>
2015-09-23 09:18:12 +0200

    sitemap v0.1

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -1,29 +1,9 @@
 # Set the host name for URL creation
+# see https://github.com/kjvarga/sitemap_generator for documentation
+
 SitemapGenerator::Sitemap.default_host = Vish::Application.config.APP_CONFIG["domain"]
 
-SitemapGenerator::Sitemap.create do
-  # Put links creation logic here.
-  #
-  # The root path '/' and sitemap index file are added automatically for you.
-  # Links are added to the Sitemap in the order they are specified.
-  #
-  # Usage: add(path, options={})
-  #        (default options are used if you don't specify)
-  #
-  # Defaults: :priority => 0.5, :changefreq => 'weekly',
-  #           :lastmod => Time.now, :host => default_host
-  #
-  # Examples:
-  #
-  # Add '/articles'
-  #
-  #   add articles_path, :priority => 0.7, :changefreq => 'daily'
-  #
-  # Add all articles:
-  #
-  #   Article.find_each do |article|
-  #     add article_path(article), :lastmod => article.updated_at
-  #   end
+SitemapGenerator::Sitemap.create do  
   priorities = { "User" => 0.9,
                  "Excursion" => 1,
                  "Workshop" => 1,
@@ -65,4 +45,27 @@ SitemapGenerator::Sitemap.create do
   add '/contest'
   add '/overview'
   add '/terms_of_use'
+
+  # Put links creation logic here.
+  #
+  # The root path '/' and sitemap index file are added automatically for you.
+  # Links are added to the Sitemap in the order they are specified.
+  #
+  # Usage: add(path, options={})
+  #        (default options are used if you don't specify)
+  #
+  # Defaults: :priority => 0.5, :changefreq => 'weekly',
+  #           :lastmod => Time.now, :host => default_host
+  #
+  # Examples:
+  #
+  # Add '/articles'
+  #
+  #   add articles_path, :priority => 0.7, :changefreq => 'daily'
+  #
+  # Add all articles:
+  #
+  #   Article.find_each do |article|
+  #     add article_path(article), :lastmod => article.updated_at
+  #   end
 end
