bffed00e68001e14898ca6c6a9a93a92612eb367
ebarra <ebarra@dit.upm.es>
2015-09-25 17:35:10 +0200

    detect mobile in vish and not scale editor

--- app/assets/javascripts/vish.utils.js
+++ app/assets/javascripts/vish.utils.js
@@ -24,14 +24,40 @@ Vish.Utils = (function(V,undefined){
       }
       
       return $("#" + inputId).val().trim() != "";
-      
+    };
 
+    /* usage:
+     * Vish.Utils.isMobile.any()
+     */
+    var isMobile = {
+      Android: function() {
+          return navigator.userAgent.match(/Android/i);
+      },
+      BlackBerry: function() {
+          return navigator.userAgent.match(/BlackBerry/i);
+      },
+      iOS: function() {
+          return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+      },
+      Opera: function() {
+          return navigator.userAgent.match(/Opera Mini/i);
+      },
+      Windows: function() {
+          return navigator.userAgent.match(/IEMobile/i);
+      },
+      any: function() {
+          return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
+      }
     };
 
     return {
       init : init,
+      isMobile : isMobile,
       validateInput : validateInput,
       convertToTagsArray : convertToTagsArray     
     };
+
+    
+    
 
 }) (Vish);
