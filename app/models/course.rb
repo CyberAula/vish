bf93eba1a4a3b6efa520176db273b6c4a75dad85
ebarra <ebarra@dit.upm.es>
2016-02-26 13:48:03 +0100

    initializers for course

--- app/models/course.rb
+++ app/models/course.rb
@@ -4,9 +4,15 @@ class Course < ActiveRecord::Base
   has_attached_file :attachment,
                     :url => '/courses/:id/attachment',                    
                     :path => ':rails_root/documents/:class/attachments/:id_partition/:filename.:extension'
+  after_save :update_course_count
 
   def thumbnail_url
     self.getAvatarUrl || "/assets/logos/original/default_course.png"
   end
   
+  private
+
+  def update_course_count
+  	Vish::Application.config.courses_count = Course.count
+  end
 end
---
+++ config/initializers/courses.rb
@@ -0,0 +1,18 @@
+#detect if we have courses, we need courses in models available in application_config.yml 
+#and we need moodle_url in application_config.yml 
+#and at least one created course (this param will be updated when a course is created)
+Vish::Application.configure do
+  config.after_initialize do
+
+		config.courses_count  = 0
+		config.courses_enabled = false
+
+		if ActiveRecord::Base.connection.table_exists?('courses')
+			config.courses_count = Course.count
+			if Vish::Application.config.APP_CONFIG["moodle_url"].present? && (Vish::Application.config.APP_CONFIG["models"]["available"].include? "Course")
+				config.courses_enabled = true
+			end
+		end
+
+  end
+end
