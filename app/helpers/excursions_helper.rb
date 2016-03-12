456b061e7ac67386f3cc553d4e8d48447587583b
Aldo <agordillos@gmail.com>
2016-03-12 16:00:03 +0100

    Avatar URLs. HTTPs.

--- app/helpers/excursions_helper.rb
+++ app/helpers/excursions_helper.rb
@@ -1,21 +1,7 @@
 module ExcursionsHelper
   
-  def excursion_thumb_for(excursion,size=nil)
-    return image_tag("/assets/icons/draft.jpg") if excursion.draft
-    image_tag (excursion.thumbnail_url || "/assets/logos/original/excursion-00.png")
-  end
-
   def excursion_raw_thumbail(excursion)
-    excursion.thumbnail_url || "/assets/logos/original/excursion-00.png"
-  end
-
-  def JSON_excursion?(excursion,param)
-  		if JSON(excursion.json)[param] == "false"
-  			response = false
-  		else
-  			response = true
-  		end
-  		response
+    Embed.checkUrlProtocol(excursion.thumbnail_url,request.protocol) || "/assets/logos/original/excursion-00.png"
   end
 
 end
