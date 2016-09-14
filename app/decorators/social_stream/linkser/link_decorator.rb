6d4d88cbc21fb24591c849c41068ecf74f1dd2cd
ebarra <ebarra@dit.upm.es>
2016-09-14 08:37:38 +0200

    links v1

---
+++ app/decorators/social_stream/linkser/link_decorator.rb
@@ -0,0 +1,18 @@
+Link.class_eval do
+
+  #method that returns a processed url with some fixes
+  #adds http if not present
+  #fixes youtube, prezi, ... if the url is not embedable
+  def getProcessedUrl
+    final_url = url.start_with?('http') ? url : "http://"+url
+    if final_url.start_with?('https://www.youtube.com/watch?v=') || final_url.start_with?('http://www.youtube.com/watch?v=')
+      final_url = final_url.sub '/watch?v=', '/embed/'
+    elsif final_url.start_with?('https://prezi.com/') && !final_url.start_with?('https://prezi.com/embed/')
+      #we remove "https://prezi.com/" and from the last "/" to the end to get the video identifier
+      video = final_url[18..final_url.index("/",18)-1]
+      final_url = "https://prezi.com/embed/" + video
+    end
+    return final_url
+  end
+
+end
