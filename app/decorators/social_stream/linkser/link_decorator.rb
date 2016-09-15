4d9122b52dcfd072eb3f88188a4688dab19fc53a
ebarra <ebarra@dit.upm.es>
2016-09-15 13:55:09 +0200

    improvements in link detection

--- app/decorators/social_stream/linkser/link_decorator.rb
+++ app/decorators/social_stream/linkser/link_decorator.rb
@@ -4,17 +4,29 @@ Link.class_eval do
   #adds http if not present
   #fixes youtube, prezi, ... if the url is not embedable
   def getProcessedUrl
-    final_url = url.start_with?('http') ? url : "http://"+url
-    if final_url.start_with?('https://www.youtube.com/watch?v=') || final_url.start_with?('http://www.youtube.com/watch?v=')
-      final_url = final_url.sub '/watch?v=', '/embed/'
-    elsif final_url.start_with?('https://prezi.com/') && !final_url.start_with?('https://prezi.com/embed/')
-      #we remove "https://prezi.com/" and from the last "/" to the end to get the video identifier
-      video = final_url[18..final_url.index("/",18)-1]
-      final_url = "https://prezi.com/embed/" + video
-    elsif (final_url.start_with?('http://slides.com/') || final_url.start_with?('slides.com/')) && !final_url.end_with?('/embed')
-      final_url = final_url[0..-3] + "/embed"
+    uri = URI(getUrlWithProtocol)
+    myparams = uri.query ? CGI::parse(uri.query) : []
+    final_url = ""
+    case
+    when uri.host[/youtu.be|youtube.com/] && uri.path.start_with?("/watch")
+      final_url = uri.scheme + "://" + uri.host + "/embed/" + myparams["v"][0]
+    when uri.host[/prezi.com/] && !uri.path.start_with?("/embed/")
+      presentation = uri.path[1..uri.path.index("/",1)-1]
+      final_url = uri.scheme + "://" + uri.host + "/embed/" + presentation
+    when uri.host[/slides.com/]  && !uri.path.end_with?("/embed")
+      final_url = uri.scheme + "://" + uri.host +  uri.path.sub("#/","") + "/embed"
+    when uri.host[/vimeo.com/]
+      final_url = uri.scheme + "://" + "player." + uri.host + "/video" + uri.path
+    else
+      final_url = url
     end
+
     return final_url
+  end
+
+  def getUrlWithProtocol
+     final_url = link.url.start_with?('http') ? link.url : "http://"+link.url
+     final_url
   end
 
 end
