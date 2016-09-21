777d4214bdee36b3e7332deab013081c9b6efd8e
agordillo <agordillos@gmail.com>
2016-09-21 12:39:31 +0200

    Exception notifier fix

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -12,6 +12,10 @@ module ApplicationHelper
 		end
 	end
 
+	def safe_encode(value)
+		value.encode("utf-8", invalid: :replace, undef: :replace, replace: "_")
+	end
+
 	def popular_entities(number=20,models)
 		models = VishConfig.getAllResourceModels if models.nil?
 		ActivityObject.getPopular(number,{:models=>models, :actor=>current_subject, :random=>true})
