bf68abb523fd7248f2637e8cbe4de83d852e0297
ebarra <ebarra@dit.upm.es>
2018-11-14 14:33:02 +0100

    v0 oauth2

---
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -0,0 +1,19 @@
+class OmniauthCallbacksController < Devise::OmniauthCallbacksController
+  def idm
+    #binding.pry
+    # You need to implement the method below in your model (e.g. app/models/user.rb)
+    @user = User.from_omniauth(request.env["omniauth.auth"])
+
+    if @user.persisted?
+      sign_in_and_redirect @user, event: :authentication #this will throw if @user is not activated
+      set_flash_message(:notice, :success, kind: "Facebook") if is_navigational_format?
+    else
+      session["devise.idm_data"] = request.env["omniauth.auth"]
+      redirect_to new_user_registration_url
+    end
+  end
+
+  def failure
+    redirect_to root_path
+  end
+end
