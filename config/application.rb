cfdea12514e3e8db04e70d55c0b3fa9cbe91a17b
agordillo <agordillos@gmail.com>
2020-03-06 14:16:16 +0100

    Include Rack::Cors gem for handling Cross-Origin Resource Sharing (CORS)

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,15 @@ end
 
 module Vish
   class Application < Rails::Application
+
+    #Handler for Cross-Origin Resource Sharing (CORS)
+    config.middleware.insert_before 0, "Rack::Cors" do
+      allow do
+        origins '*'
+        resource '*', headers: :any, methods: [:get, :post, :options]
+      end
+    end
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
